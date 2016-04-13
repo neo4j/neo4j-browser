pe4cey/neo4j-browser-old

Create href's from strings in table results
Uses regex to make strings that contain hyperlinks in to href's

app/scripts/directives/neoTable.coffee
@@ -42,7 +42,7 @@ angular.module('neo4jApp.directives')
         cell2html = (cell) ->
           if angular.isString(cell)
             return emptyMarker() unless cell.length
-            Utils.escapeHTML(cell)
+            Utils.replaceHyperlink(cell)
           else if angular.isArray(cell)
             "["+((cell2html(el) for el in cell).join(', '))+"]"
           else if angular.isObject(cell)

neo4j/neo4j-browser

app/scripts/directives/neoTable.coffee
@@ -42,7 +42,7 @@ angular.module('neo4jApp.directives')
         cell2html = (cell) ->
           if angular.isString(cell)
             return emptyMarker() unless cell.length
-            Utils.escapeHTML(cell)
+            Utils.replaceHyperlinkOrEncodeHTML(cell)
           else if angular.isArray(cell)
             "["+((cell2html(el) for el in cell).join(', '))+"]"
           else if angular.isObject(cell)