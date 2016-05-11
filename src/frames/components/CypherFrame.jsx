5f4667944f29d66f860b26d416a6c7ecc7075a50
Oskar Hane <oh@oskarhane.com>
2016-05-11 23:31:04 +0200

    Render query results in ascii-data-table in frames

--- src/frames/components/CypherFrame.jsx
+++ src/frames/components/CypherFrame.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import asciitable from 'ascii-data-table'
+import bolt from '../../services/bolt'
 
 const CypherFrame = ({frame}) => {
   const errors = frame.errors && frame.errors.fields || false
   const result = frame.result || false
-  let frameContents = JSON.stringify(result)
-  if (errors) {
+  let frameContents = <pre>{JSON.stringify(result, null, 2)}</pre>
+  if (result) {
+    const rows = bolt.recordsToTableArray(result.records)
+    frameContents = <pre>{asciitable.run(rows)}</pre>
+  } else if (errors) {
     frameContents = (
       <div>
         {errors[0].code}
--- src/services/bolt.js
+++ src/services/bolt.js
@@ -26,7 +26,38 @@ function transaction (input, parameters) {
   })
 }
 
+function recordsToTableArray (records) {
+  const recordValues = records.map((record) => {
+    let out = []
+    record.forEach((val, key) => out.push(itemIntToString(val)))
+    return out
+  })
+  const keys = records[0].keys
+  return [[...keys], ...recordValues]
+}
+
+function itemIntToString (item) {
+  if (Array.isArray(item)) return arrayIntToString(item)
+  if (['number', 'string', 'boolean'].indexOf(typeof item) !== -1) return item
+  if (item === null) return item
+  if (neo4j.v1.isInt(item)) return item.toString()
+  if (typeof item === 'object') return objIntToString(item)
+}
+
+function arrayIntToString (arr) {
+  return arr.map((item) => itemIntToString(item))
+}
+
+function objIntToString (obj) {
+  Object.keys(obj).forEach((key) => {
+    obj[key] = itemIntToString(obj[key])
+  })
+  return obj
+}
+
 export default {
   transaction,
-  getInstance
+  getInstance,
+  recordsToTableArray,
+  neo4j: neo4j.v1
 }
