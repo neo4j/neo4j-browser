0e8c1187d1c8e1eaee8ced4c4f82f287128f05a0
Oskar Hane <oh@oskarhane.com>
2016-05-17 16:11:32 +0200

    Walk history in editor with cmd+up/down
    Not added tests for this yet because not sure how to.

--- src/editor/components/Editor.jsx
+++ src/editor/components/Editor.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import editor from '../'
+import { getHistory, getEditorContent } from '../../selectors'
 import Codemirror from 'react-codemirror'
 import 'codemirror/mode/cypher/cypher'
 import 'codemirror/lib/codemirror.css'
@@ -10,7 +11,9 @@ export class EditorComponent extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      code: ''
+      code: '',
+      historyIndex: -1,
+      buffer: null
     }
   }
   handleEnter (cm) {
@@ -24,8 +27,28 @@ export class EditorComponent extends React.Component {
   }
   execCurrent (cm) {
     this.props.onExecute(cm.getValue())
-    cm.setValue('')
-    this.updateCode('')
+    this.setEditorValue(cm, '')
+    this.setState({historyIndex: -1, buffer: null})
+  }
+  historyPrev (cm) {
+    if (!this.props.history.length) return
+    if (this.state.historyIndex + 1 === this.props.history.length) return
+    if (this.state.historyIndex === -1) { // Save what's currently in the editor
+      this.setState({buffer: cm.getValue()})
+    }
+    this.setState({historyIndex: this.state.historyIndex + 1})
+    this.setEditorValue(cm, this.props.history[this.state.historyIndex].cmd)
+  }
+  historyNext (cm) {
+    if (!this.props.history.length) return
+    if (this.state.historyIndex <= -1) return
+    if (this.state.historyIndex === 0) { // Should read from buffer
+      this.setState({historyIndex: -1})
+      this.setEditorValue(cm, this.state.buffer)
+      return
+    }
+    this.setState({historyIndex: this.state.historyIndex - 1})
+    this.setEditorValue(cm, this.props.history[this.state.historyIndex].cmd)
   }
   componentWillReceiveProps (nextProps) {
     if (nextProps.content !== this.state.code) {
@@ -39,6 +62,14 @@ export class EditorComponent extends React.Component {
     this.codeMirrorInstance.keyMap['default']['Shift-Enter'] = this.newlineAndIndent.bind(this)
     this.codeMirrorInstance.keyMap['default']['Cmd-Enter'] = this.execCurrent.bind(this)
     this.codeMirrorInstance.keyMap['default']['Ctrl-Enter'] = this.execCurrent.bind(this)
+    this.codeMirrorInstance.keyMap['default']['Cmd-Up'] = this.historyPrev.bind(this)
+    this.codeMirrorInstance.keyMap['default']['Ctrl-Up'] = this.historyPrev.bind(this)
+    this.codeMirrorInstance.keyMap['default']['Cmd-Down'] = this.historyNext.bind(this)
+    this.codeMirrorInstance.keyMap['default']['Ctrl-Down'] = this.historyNext.bind(this)
+  }
+  setEditorValue (cm, cmd) {
+    cm.setValue(cmd)
+    this.updateCode(cmd)
   }
   updateCode (newCode) {
     this.setState({
@@ -77,7 +108,8 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state) => {
   return {
-    content: state.editor.content
+    content: getEditorContent(state),
+    history: getHistory(state)
   }
 }
 
--- src/selectors.js
+++ src/selectors.js
@@ -1,2 +1,3 @@
 export const getSettings = (state) => state.settings
 export const getHistory = (state) => state.editor.history
+export const getEditorContent = (state) => state.editor.content
