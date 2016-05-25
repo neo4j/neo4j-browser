745ad0d9e1f4edc5338b18d09ec46add6c42da73
Eve Bright <eve.bright@neotechnology.com>
2016-05-25 16:21:36 +0100

    Fix sidebar test so that it doesn't have an error

--- src/sidebar/components/Sidebar.test.jsx
+++ src/sidebar/components/Sidebar.test.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import sidebar from '../'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import dbInfo from '../dbInfo'
 
 describe('Sidebar', () => {
-  const store = createStore(sidebar.reducer)
+  const reducer = combineReducers({
+    meta: dbInfo.reducer
+  })
+  const store = createStore(reducer)
 
-  it('shopuld show db drawer whem it is open', () => {
+  it('should show db drawer whem it is open', () => {
     const drawer = 'db'
     const wrapper = mount(<Provider store={store}><sidebar.components.Sidebar openDrawer={drawer} onNavClick={() => {}}/></Provider>)
     expect(wrapper.find('#db-drawer')).has.length(1)
   })
 
-  it('shopuld show fav drawer whem it is open', () => {
+  it('should show fav drawer whem it is open', () => {
     const drawer = 'fav'
     const wrapper = mount(<Provider store={store}><sidebar.components.Sidebar openDrawer={drawer} onNavClick={() => {}}/></Provider>)
     expect(wrapper.find('.tab').first().text()).to.equal('Fav')
