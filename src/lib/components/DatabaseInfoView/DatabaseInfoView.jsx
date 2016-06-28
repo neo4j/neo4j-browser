9ac127ada325064a4695637e3cc64d87594f0ec7
Oskar Hane <oh@oskarhane.com>
2016-06-28 10:32:37 +0200

    Remove editor from dbinfo view

--- src/lib/components/DatabaseInfoView/DatabaseInfoView.jsx
+++ src/lib/components/DatabaseInfoView/DatabaseInfoView.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import editor from '../../containers/editor'
 import dbInfo from '../../containers/dbInfo'
 import styles from './style.css'
 
 const QueryView = () => {
   return (
     <div className={styles.dbinfoView}>
-      <editor.components.Editor />
       <div className={styles.dbinfoCard}>
         <dbInfo.components.DatabaseInfo />
       </div>
