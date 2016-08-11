a39a9610717cee325c0bf3487ff13ec4c2dcc759
Mark Peace <mark.peace@neotechnology.com>
2016-08-11 16:30:22 +0100

    Add more tests for carousel

--- src/guides/components/Carousel.jsx
+++ src/guides/components/Carousel.jsx
@@ -8,22 +8,19 @@ import FlatButton from 'material-ui/FlatButton'
 export class Carousel extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {loaded: false, slides: null}
+    this.state = {slides: null}
   }
   componentDidMount () {
     const slides = ReactDOM.findDOMNode(this).getElementsByTagName('slide')
-    let reactSlides = null
+    let reactSlides = this
     if (slides.length > 0) {
       reactSlides = Array.from(slides).map((slide) => {
-        const label = ReactDOM.findDOMNode(slide).getElementsByTagName('h3')[0]
         return {
-          html: slide,
-          label: label
+          html: slide
         }
       })
     }
-
-    this.setState(Object.assign(this.state, { slides: reactSlides, loaded: true }))
+    this.setState(Object.assign(this.state, { slides: reactSlides }))
   }
   next () {
     this.refs.reactSwipe.next()
@@ -32,26 +29,23 @@ export class Carousel extends React.Component {
     this.refs.reactSwipe.prev()
   }
   render () {
-    if (this.state.loaded) {
-      if (this.state.slides) {
-        const ListOfSlides = this.state.slides.map((slide) => {
-          return (
-            <div className='slide' key={uuid.v4()}>
-              <Slide html={slide.html.innerHTML}/>
-            </div>
-          )
-        })
+    if (this.state.slides && Array.isArray(this.state.slides)) {
+      const ListOfSlides = this.state.slides.map((slide) => {
         return (
-          <div className='carousel'>
-            <Slide html={''}/>
-            <FlatButton className='left-button' label='<' primary onClick={this.prev.bind(this)}/>
-            <ReactSwipe ref='reactSwipe' swipeOptions={{continuous: false}}>
-              {ListOfSlides}
-            </ReactSwipe>
-            <FlatButton className='right-button' label='>' primary onClick={this.next.bind(this)}/>
+          <div key={uuid.v4()}>
+            <Slide key={uuid.v4()} html={slide.html.innerHTML}/>
           </div>
         )
-      }
+      })
+      return (
+        <div key={uuid.v4()}className='carousel-container'>
+          <FlatButton className='left-button' label='<' primary onClick={this.prev.bind(this)}/>
+          <ReactSwipe className='carousel' ref='reactSwipe' swipeOptions={{continuous: false}}>
+            {ListOfSlides}
+          </ReactSwipe>
+          <FlatButton className='right-button' label='>' primary onClick={this.next.bind(this)}/>
+        </div>
+      )
     }
     return (
       <Slide html={this.props.html}/>
