63636255542197caa1979c1bfe91ed120b1d5854
Mark Peace <mark.peace@neotechnology.com>
2016-08-18 13:49:15 +0100

    Update icons for left hand drawer

--- src/lib/containers/sidebar/components/Sidebar.jsx
+++ src/lib/containers/sidebar/components/Sidebar.jsx
@@ -4,6 +4,14 @@ import favorites from '../favorites'
 import documents from '../documents'
 import tabNavigation from '../../tabNavigation'
 import settings from '../../settings'
+import ActionGrade from 'material-ui/svg-icons/action/grade'
+import ActionViewHeadline from 'material-ui/svg-icons/action/view-headline'
+import ActionDescription from 'material-ui/svg-icons/action/description'
+import ActionSettings from 'material-ui/svg-icons/action/settings'
+import FileCloud from 'material-ui/svg-icons/file/cloud'
+import ActionInfo from 'material-ui/svg-icons/action/info'
+
+import {white} from 'material-ui/styles/colors'
 
 import styles from './style.css'
 
@@ -15,17 +23,22 @@ class Sidebar extends React.Component {
     const FavoritesDrawer = favorites.components.Favorites
     const DocumentsDrawer = documents.components.DocumentsComponent
     const SettingsDrawer = settings.components.Settings
-    const navItemsList = [
-      {name: 'DB', icon: '', content: DatabaseDrawer},
-      {name: 'Favorites', icon: '', content: FavoritesDrawer},
-      {name: 'Documents', icon: '', content: DocumentsDrawer},
-      {name: 'Settings', icon: '', content: SettingsDrawer}
+    const topNavItemsList = [
+      {name: 'DB', icon: <ActionViewHeadline color={white}/>, content: DatabaseDrawer},
+      {name: 'Favorites', icon: <ActionGrade color={white}/>, content: FavoritesDrawer},
+      {name: 'Documents', icon: <ActionDescription color={white}/>, content: DocumentsDrawer}
+    ]
+    const bottomNavItemsList = [
+      {name: 'Sync', icon: <FileCloud color={white}/>, content: SettingsDrawer},
+      {name: 'Settings', icon: <ActionSettings color={white}/>, content: SettingsDrawer},
+      {name: 'About', icon: <ActionInfo color={white}/>, content: SettingsDrawer}
     ]
 
     return (<tabNavigation.components.Navigation
       openDrawer={openDrawer}
       onNavClick={onNavClick}
-      navItems={navItemsList}
+      topNavItems={topNavItemsList}
+      bottomNavItems={bottomNavItemsList}
       sidebarClassName={styles.sidebar}
       listClassName={styles.list}
       selectedItemClassName={styles['selected-item']}
--- src/lib/containers/tabNavigation/components/Navigation.jsx
+++ src/lib/containers/tabNavigation/components/Navigation.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import { List, ListItem } from 'material-ui/List'
+import IconButton from 'material-ui/IconButton'
 import classNames from 'classnames'
 
 class Navigation extends React.Component {
   render () {
+    const buildNavList = (list) => {
+      return list.map((item, index) => {
+        const itemClass = classNames({
+          [selectedItemClassName]: item.name.toLowerCase() === openDrawer
+        })
+        return (
+          <ListItem key={index}
+            className={itemClass}
+            onTouchTap={() => onNavClick(item.name.toLowerCase())}>
+            <IconButton tooltip={item.name}>
+              {item.icon}
+            </IconButton>
+          </ListItem>
+        )
+      })
+    }
     const openDrawer = this.props.openDrawer
     const onNavClick = this.props.onNavClick
-    const navItemsList = this.props.navItems
+    const topNavItemsList = this.props.topNavItems
+    const bottomNavItemsList = this.props.bottomNavItems
     const selectedItemClassName = this.props.selectedItemClassName
     const getContentToShow = (openDrawer) => {
       if (openDrawer) {
-        let filteredList = navItemsList.filter((item) => {
+        let filteredList = topNavItemsList.concat(bottomNavItemsList).filter((item) => {
           return item.name.toLowerCase() === openDrawer
         })
         let TabContent = filteredList[0].content
@@ -18,24 +36,18 @@ class Navigation extends React.Component {
       }
       return null
     }
-    const navItems = navItemsList.map((item, index) => {
-      const itemClass = classNames({
-        [selectedItemClassName]: item.name.toLowerCase() === openDrawer
-      })
-      return <ListItem
-        onTouchTap={() => onNavClick(item.name.toLowerCase())}
-        key={index}
-        className={itemClass}
-        primaryText={item.name}
-      />
-    })
+    const topNavItems = buildNavList(topNavItemsList)
+    const bottomNavItems = buildNavList(bottomNavItemsList)
     const tabClass = classNames({
       hidden: !openDrawer,
       [this.props.tabClassName]: true
     })
     return (
       <div className={this.props.sidebarClassName}>
-        <List className={this.props.listClassName}>{navItems}</List>
+        <List className={this.props.listClassName}>
+          <List className={this.props.listClassName}>{topNavItems}</List>
+          <List className={this.props.listClassName}>{bottomNavItems}</List>
+        </List>
         <div className={tabClass + ' tab'}>
           {getContentToShow(openDrawer)}
         </div>
