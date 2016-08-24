a4b619249e73972f517c4fe82b66e4e7472f96c4
Eve Bright <eve.bright@neotechnology.com>
2016-08-24 13:07:35 +0100

    Added some tests around style frame

--- src/lib/containers/visualization/components/index.js
+++ src/lib/containers/visualization/components/index.js
@@ -1,4 +1,5 @@
 import { Explorer } from './Explorer'
 import { GrassEditor } from './GrassEditor'
+import { StyleEditor } from './StyleEditor'
 
-export default { Explorer, GrassEditor }
+export default { Explorer, GrassEditor, StyleEditor }
