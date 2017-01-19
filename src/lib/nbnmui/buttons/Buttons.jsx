56b522bb01fcfe0f417b2a54f8cd97efba78bac3
Mark Peace <mark.peace@neotechnology.com>
2017-01-19 16:01:25 +0000

    Refactor ToolTip to be a higher order component

--- src/lib/nbnmui/buttons/Buttons.jsx
+++ src/lib/nbnmui/buttons/Buttons.jsx
@@ -2,41 +2,17 @@ import React from 'react'
 import ListItem from 'grommet/components/ListItem'
 import Button from 'grommet/components/Button'
 import Tip from 'grommet/components/Tip'
+import uuid from 'uuid'
 import styles from './style.css'
 
-export const CloseButton = (props) => {
-  return (
-    <Button {...props} className={styles.icon}>×</Button>
-  )
-}
-export const EditorButton = (props) => {
-  return (
-    <Button {...props} />
-  )
-}
-export const FavoriteItem = (props) => {
-  const primaryText = (<p>{props.primaryText}</p>)
-  const rightIcon = (props.removeClick) ? (<CloseButton onClick={props.removeClick} />) : null
-  return (
-    <ListItem className='remove'>
-      <span {...props}>
-        {primaryText}
-      </span>
-      <span>
-        {rightIcon}
-      </span>
-    </ListItem>
-  )
-}
-
-export class NavigationButton extends React.Component {
+export class ToolTip extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       mouseover: false,
       id: props.id,
       icon: props.icon,
-      name: props.name,
+      name: props.tooltip,
       hidden: true
     }
   }
@@ -48,12 +24,49 @@ export class NavigationButton extends React.Component {
   }
   render () {
     const tooltip = (this.state.mouseover) ? <Tip className={this.state.hidden} onClose={() => {}} target={this.state.id}>{this.state.name}</Tip> : null
-
     return (
       <div onMouseLeave={this.onMouseLeaveHandler.bind(this)} onMouseEnter={this.onMouseEnterHandler.bind(this)}>
-        <Button {...this.props} id={this.state.id} icon={this.state.icon} />
+        {this.props.children}
         {tooltip}
       </div>
     )
   }
+}
+
+export const CloseButton = (props) => {
+  return (
+    <Button {...props} className={styles.icon}>×</Button>
+  )
+}
+export const EditorButton = (props) => {
+  const {tooltip, ...rest} = props
+  const id = uuid.v4()
+  const button = <Button id={id} {...rest} />
+  return (
+    <ToolTip id={id} tooltip={tooltip}>
+      {button}
+    </ToolTip>
+  )
+}
+export const FavoriteItem = (props) => {
+  const {primaryText, removeClick, ...rest} = props
+  const rightIcon = (removeClick) ? (<CloseButton onClick={props.removeClick} />) : null
+  return (
+    <ListItem className='remove'>
+      <span {...rest}>
+        <p>{primaryText}</p>
+      </span>
+      <span>
+        {rightIcon}
+      </span>
+    </ListItem>
+  )
+}
+
+export const NavigationButton = (props) => {
+  return (
+    <ToolTip id={props.id} tooltip={props.name}>
+      <Button {...props} id={props.id} icon={props.icon} />
+    </ToolTip>
+  )
 }
