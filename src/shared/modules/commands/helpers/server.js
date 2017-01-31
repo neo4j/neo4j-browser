8b1653c57d8aac1f424572e3eab6460c77344aa7
Oskar Hane <oh@oskarhane.com>
2017-01-31 15:13:02 +0100

    :server add

--- src/shared/modules/commands/helpers/server.js
+++ src/shared/modules/commands/helpers/server.js
@@ -1,7 +1,7 @@
-import { splitStringOnFirst } from 'services/commandUtils'
+import { splitStringOnFirst, splitStringOnLast } from 'services/commandUtils'
 import bolt from 'services/bolt/bolt'
 import * as connections from 'shared/modules/connections/connectionsDuck'
-import { UnknownCommandError, getErrorMessage, ConnectionNotFoundError } from 'services/exceptions'
+import { UnknownCommandError, getErrorMessage, ConnectionNotFoundError, AddServerValidationError } from 'services/exceptions'
 import { add as addFrame } from 'shared/modules/stream/streamDuck'
 
 export function handleServerCommand (action, cmdchar, put, store) {
@@ -12,6 +12,10 @@ export function handleServerCommand (action, cmdchar, put, store) {
   }
   if (serverCmd === 'connect') {
     connectToConnection(action, props, put, store)
+    return
+  }
+  if (serverCmd === 'add') {
+    handleServerAddCommand(action, cmdchar, put, store)
     return
   }
   put(addFrame({...action, type: 'error', error: {message: getErrorMessage(UnknownCommandError(action.cmd))}}))
@@ -40,4 +44,24 @@ function connectToConnection (action, connectionName, put, store) {
   } catch (e) {
     put(addFrame({...action, type: 'error', error: {message: getErrorMessage(e)}}))
   }
+}
+
+function handleServerAddCommand (action, cmdchar, put, store) {
+  // :server add name username:password@host:port
+  const [serverCmd, props] = splitStringOnFirst(splitStringOnFirst(action.cmd.substr(cmdchar.length), ' ')[1], ' ')
+  const [name, creds] = splitStringOnFirst(props, ' ')
+  const [userCreds, host] = splitStringOnLast(creds, '@')
+  const [username, password] = splitStringOnFirst(userCreds, ':')
+  try {
+    if (!serverCmd || !props) throw new AddServerValidationError()
+    if (!name || !creds) throw new AddServerValidationError()
+    if (!userCreds || !host) throw new AddServerValidationError()
+    if (!username || !password) throw new AddServerValidationError()
+  } catch (e) {
+    put(addFrame({...action, type: 'error', error: {message: getErrorMessage(e)}}))
+    return
+  }
+  put(connections.addConnection({name, username, password, host}))
+  const state = store.getState()
+  put(addFrame({...action, type: 'pre', result: JSON.stringify(connections.getConnections(state), null, 2)}))
 }
