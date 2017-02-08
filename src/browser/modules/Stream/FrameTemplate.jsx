877e895841fa3903657336a6ff86736738d1f73c
Mark Peace <mark.peace@neotechnology.com>
2017-02-08 09:07:44 +0000

    Afix titlebar to top of a result frame

--- src/browser/modules/Stream/FrameTemplate.jsx
+++ src/browser/modules/Stream/FrameTemplate.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import Card from 'grommet/components/Card'
+import Article from 'grommet/components/Article'
+import Header from 'grommet/components/Header'
+import Section from 'grommet/components/Section'
 
 import styles from './style_frame.css'
 
 const FrameTemplate = ({header, contents}) => {
   return (
-    <Card className={styles.frame} heading={header}>
-      <div className={styles.contents + ' frame-contents'}>
+    <Article className={styles.frame}>
+      <Header className={styles.frameHeader}>
+        {header}
+      </Header>
+      <Section className={styles.contents + ' frame-contents'}>
         {contents}
-      </div>
-    </Card>
+      </Section>
+    </Article>
   )
 }
 
