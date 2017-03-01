3a7f6acfc6bad2fe09f0b51274a857da58c311ec
Mark Peace <mark.peace@neotechnology.com>
2017-03-01 09:53:01 +0100

    Remove grommet from ConnectForm

--- src/browser/modules/Stream/Auth/ConnectForm.jsx
+++ src/browser/modules/Stream/Auth/ConnectForm.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
 
-import Form from 'grommet/components/Form'
-import FormField from 'grommet/components/FormField'
-import FormFields from 'grommet/components/FormFields'
-import Footer from 'grommet/components/Footer'
 import {FormButton} from 'nbnmui/buttons'
-import TextInput from 'grommet/components/TextInput'
 
 const ConnectForm = (props) => (
   <div>
-    <Form>
-      <FormFields>
-        <FormField label='Host'>
-          <TextInput onDOMChange={props.onHostChange} value={props.host} />
-        </FormField>
-        <FormField label='Username'>
-          <TextInput onDOMChange={props.onUsernameChange} value={props.username} />
-        </FormField>
-        <FormField label='Password'>
-          <TextInput type='password' onDOMChange={props.onPasswordChange} value={props.password} />
-        </FormField>
-      </FormFields>
-    </Form>
-    <Footer>
+    <form>
+      <ul>
+        <li>
+          <label>Host</label>
+          <input onChange={props.onHostChange} value={props.host} />
+        </li>
+        <li>
+          <label>Username</label>
+          <input onChange={props.onUsernameChange} value={props.username} />
+        </li>
+        <li>
+          <label>Password</label>
+          <input onChange={props.onPasswordChange} value={props.password} />
+        </li>
+      </ul>
       <FormButton onClick={props.onConnectClick}>Connect</FormButton>
-    </Footer>
+    </form>
   </div>
 )
 
