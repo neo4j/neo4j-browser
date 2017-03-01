29173277a762838a615ab814c15b8bb208783dcd
Mark Peace <mark.peace@neotechnology.com>
2017-03-01 18:49:53 +0100

    Remove grommet from Toolbar

--- src/browser/components/FileImporter/FileDropBar.jsx
+++ src/browser/components/FileImporter/FileDropBar.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import {Toolbar, ToolbarTitle, ToolbarGroup} from 'grommet/components/Toolbar'
 import {FileDrop} from './FileDrop'
 
 const DropBar = (props) => {
-  return (<div>
-    <Toolbar>
-      <ToolbarGroup>
-        <ToolbarTitle text={props.text} />
-      </ToolbarGroup>
-    </Toolbar>
-  </div>)
+  return (<div>{props.text}</div>)
 }
 
 export const FileDropBar = FileDrop(DropBar, true)
