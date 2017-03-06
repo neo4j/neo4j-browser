1f2c320323943cd045f27518f12a1b2a13d65acf
Oskar Hane <oh@oskarhane.com>
2017-03-06 16:49:27 +0100

    Add utility method to move items in an array

--- src/shared/services/utils.js
+++ src/shared/services/utils.js
@@ -6,5 +6,17 @@ export const deepEquals = (x, y) => {
   return (x === y)
 }
 
+export const moveInArray = (fromIndex, toIndex, arr) => {
+  if (!Array.isArray(arr)) return false
+  if (fromIndex < 0 || fromIndex >= arr.length) return false
+  if (toIndex < 0 || toIndex >= arr.length) return false
+
+  const newArr = [].concat(arr)
+  const el = arr[fromIndex]
+  newArr.splice(fromIndex, 1)
+  newArr.splice(toIndex, 0, el)
+  return newArr
+}
+
 // Epic helpers
 export const put = (dispatch) => (action) => dispatch(action)
--- src/shared/services/utils.test.js
+++ src/shared/services/utils.test.js
@@ -1,4 +1,4 @@
-/* global test, expect */
+/* global describe, test, expect */
 import * as utils from './utils'
 
 describe('utils', () => {
@@ -11,5 +11,22 @@ describe('utils', () => {
     // When & Then
     expect(utils.deepEquals(o1, o2)).toBeTruthy()
     expect(utils.deepEquals(o1, o3)).toBeFalsy()
+  })
+  test('can move items in an array', () => {
+    // Given
+    const tests = [
+      { test: [1, 2, 3], from: -1, to: 1, expect: false },
+      { test: [1, 2, 3], from: 0, to: 3, expect: false },
+      { test: [1, 2, 3], from: 5, to: 1, expect: false },
+      { test: 'string', from: 0, to: 3, expect: false },
+      { test: [1, 2, 3], from: 0, to: 1, expect: [2, 1, 3] },
+      { test: [1, 2, 3], from: 2, to: 1, expect: [1, 3, 2] },
+      { test: [1, 2, 3], from: 2, to: 0, expect: [3, 1, 2] }
+    ]
+
+    // When && Then
+    tests.forEach((t) => {
+      expect(utils.moveInArray(t.from, t.to, t.test)).toEqual(t.expect)
+    })
   })
 })
