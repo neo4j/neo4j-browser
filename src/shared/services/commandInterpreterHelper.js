b7702b9e01812d18cea50cde80217c52f72288b4
Oskar Hane <oh@oskarhane.com>
2017-03-07 15:01:46 +0100

    Read server config whitelist and see if allowed

--- src/shared/services/commandInterpreterHelper.js
+++ src/shared/services/commandInterpreterHelper.js
@@ -3,7 +3,9 @@ import { getHistory } from 'shared/modules/history/historyDuck'
 import { update as updateQueryResult } from 'shared/modules/requests/requestsDuck'
 import { getParams } from 'shared/modules/params/paramsDuck'
 import { cleanHtml } from 'services/remoteUtils'
+import { hostIsAllowed } from 'services/utils'
 import remote from 'services/remote'
+import { getServerConfig } from 'services/bolt/boltHelpers'
 import { handleServerCommand } from 'shared/modules/commands/helpers/server'
 import { handleCypherCommand } from 'shared/modules/commands/helpers/cypher'
 import { handleParamCommand, handleParamsCommand } from 'shared/modules/commands/helpers/params'
@@ -78,13 +80,20 @@ const availableCommands = [{
   match: (cmd) => /^play(\s|$)https?/.test(cmd),
   exec: function (action, cmdchar, put, store) {
     const url = action.cmd.substr(cmdchar.length + 'play '.length)
-    try {
-      remote.get(url).then((r) => {
-        put(frames.add({...action, type: 'play-remote', result: cleanHtml(r)}))
-      })
-    } catch (e) {
-      put(frames.add({...action, type: 'play-remote', error: CouldNotFetchRemoteGuideError(e)}))
-    }
+    getServerConfig().then((conf) => {
+      const whitelist = conf && conf['browser.remote_content_hostname_whitelist']
+      if (!hostIsAllowed(url, whitelist.value)) {
+        throw new Error('Hostname is not allowed according to server whitelist')
+      }
+      remote.get(url)
+        .then((r) => {
+          put(frames.add({...action, type: 'play-remote', result: cleanHtml(r)}))
+        }).catch((e) => {
+          put(frames.add({...action, type: 'play-remote', error: CouldNotFetchRemoteGuideError(e.name + ': ' + e.message)}))
+        })
+    }).catch((e) => {
+      put(frames.add({...action, type: 'play-remote', error: CouldNotFetchRemoteGuideError(e.name + ': ' + e.message)}))
+    })
   }
 }, {
   name: 'play',
