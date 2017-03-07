c4038aa60cc186dc1ffb5eb52161c861e870f599
Oskar Hane <oh@oskarhane.com>
2017-03-07 08:44:13 +0100

    Populate editor on app start

--- src/shared/rootEpic.js
+++ src/shared/rootEpic.js
@@ -6,6 +6,7 @@ import { dbMetaEpic, clearMetaOnDisconnectEpic } from './modules/dbMeta/dbMetaDu
 import { cancelRequestEpic } from './modules/requests/requestsDuck'
 import { discoveryOnStartupEpic } from './modules/discovery/discoveryDuck'
 import { clearLocalstorageEpic } from './modules/localstorage/localstorageDuck'
+import { populateEditorFromUrlEpic } from './modules/editor/editorDuck'
 import { cypherRequestEpic, handleForcePasswordChangeEpic } from './modules/cypher/cypherDuck'
 
 export default combineEpics(
@@ -21,6 +22,7 @@ export default combineEpics(
   clearMetaOnDisconnectEpic,
   cancelRequestEpic,
   discoveryOnStartupEpic,
+  populateEditorFromUrlEpic,
   cypherRequestEpic,
   clearLocalstorageEpic,
   handleForcePasswordChangeEpic
