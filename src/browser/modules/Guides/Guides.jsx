9f0ab5e72c8fef69f6eb5e5b404360b22c26029f
Mark Peace <mark.peace@neotechnology.com>
2017-03-08 14:06:25 +0000

    Fix usage of ref in a ReactSwipe component

--- src/browser/modules/Guides/Guides.jsx
+++ src/browser/modules/Guides/Guides.jsx
@@ -28,10 +28,10 @@ export default class Guides extends Component {
     this.setState({ slides: reactSlides, firstRender: false })
   }
   next () {
-    this.refs.reactSwipe.next()
+    this.carousel.next()
   }
   prev () {
-    this.refs.reactSwipe.prev()
+    this.carousel.prev()
   }
   render () {
     if (this.state.slides && Array.isArray(this.state.slides)) {
@@ -54,7 +54,7 @@ export default class Guides extends Component {
       return (
         <div key={uuid.v4()} className={styles.carouselContainer}>
           <button className={styles.leftButton} onClick={this.prev.bind(this)}>{'<'}</button>
-          <ReactSwipe className={styles.carousel} ref='reactSwipe' swipeOptions={{continuous: false}}>
+          <ReactSwipe className={styles.carousel} ref={(ref) => { this.carousel = ref }} swipeOptions={{continuous: false}}>
             {ListOfSlides}
           </ReactSwipe>
           <button className={styles.rightButton} onClick={this.next.bind(this)}>{'>'}</button>
