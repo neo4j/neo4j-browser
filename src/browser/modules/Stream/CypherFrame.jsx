a7a95e7fa66b9a2ae868b4f45c5ae431e43c3426
Irfan Nuri Karaca <irfannuri@gmail.com>
2017-03-10 14:51:32 +0000

    Resolve EXPLAIN command issue

--- src/browser/modules/Stream/CypherFrame.jsx
+++ src/browser/modules/Stream/CypherFrame.jsx
@@ -27,8 +27,11 @@ class CypherFrame extends Component {
     let plan
     let nodesAndRelationships
     if (nextProps.request.status === 'success' && nextProps.request.result !== this.props.request.result) {
-      nodesAndRelationships = bolt.extractNodesAndRelationshipsFromRecords(nextProps.request.result.records)
-      rows = bolt.recordsToTableArray(nextProps.request.result.records)
+      if (nextProps.request.result.records && nextProps.request.result.records.length > 0) {
+        nodesAndRelationships = bolt.extractNodesAndRelationshipsFromRecords(nextProps.request.result.records)
+        rows = bolt.recordsToTableArray(nextProps.request.result.records)
+      }
+
       plan = bolt.extractPlan(nextProps.request.result)
     } else {
       this.setState({nodesAndRelationships, rows, plan})
@@ -62,27 +65,26 @@ class CypherFrame extends Component {
     const requestStatus = this.props.request.status
 
     let frameContents = <pre>{JSON.stringify(result, null, 2)}</pre>
-    if (result.records && result.records.length > 0) {
-      if (plan) {
-        frameContents = <QueryPlan plan={plan} />
-      } else {
-        this.state.rows = this.state.rows || bolt.recordsToTableArray(result.records)
-        switch (this.state.openView) {
-          case 'text':
-            frameContents = <AsciiView rows={this.state.rows} />
-            break
-          case 'table':
-            frameContents = <TableView data={this.state.rows} />
-            break
-          case 'visualization':
-            frameContents = <Visualization records={result.records} />
-            break
-          case 'code':
-            frameContents = <CodeView query={this.props.frame.cmd} request={this.props.request} />
-            break
-          default:
-            frameContents = <Visualization records={result.records} />
-        }
+
+    if (plan) {
+      frameContents = <QueryPlan plan={plan} />
+    } else if (result.records && result.records.length > 0) {
+      this.state.rows = this.state.rows || bolt.recordsToTableArray(result.records)
+      switch (this.state.openView) {
+        case 'text':
+          frameContents = <AsciiView rows={this.state.rows} />
+          break
+        case 'table':
+          frameContents = <TableView data={this.state.rows} />
+          break
+        case 'visualization':
+          frameContents = <Visualization records={result.records} />
+          break
+        case 'code':
+          frameContents = <CodeView query={this.props.frame.cmd} request={this.props.request} />
+          break
+        default:
+          frameContents = <Visualization records={result.records} />
       }
     } else if (errors) {
       frameContents = (
