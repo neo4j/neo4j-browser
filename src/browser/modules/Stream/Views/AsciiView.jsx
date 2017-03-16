276f2e99bdebf1245425a9075895cc4f87659309
Oskar Hane <oh@oskarhane.com>
2017-03-16 10:43:27 +0100

    Make asciitable render wider columns

--- src/browser/modules/Stream/Views/AsciiView.jsx
+++ src/browser/modules/Stream/Views/AsciiView.jsx
@@ -1,7 +1,7 @@
 import asciitable from 'ascii-data-table'
 
 const AsciiView = ({rows}) => {
-  return <pre>{asciitable.table(rows)}</pre>
+  return <pre>{asciitable.table(rows, 70)}</pre>
 }
 
 export default AsciiView
