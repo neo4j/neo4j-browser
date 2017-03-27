75394e84d2f2f997d7c4b26b0ded61e3918cbea5
Mark Peace <mark.peace@neotechnology.com>
2017-03-27 10:24:07 +0100

    Add slide out animation for side drawers

--- src/browser/components/TabNavigation/styled.jsx
+++ src/browser/components/TabNavigation/styled.jsx
@@ -1,8 +1,19 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const pushRight = keyframes`
+  from {
+    transform: translate(0, 0);
+    width: 0px;
+  }
+  to {
+    opacity: 1;
+  }
+`
 
 export const StyledDrawer = styled.div`
   flex: 0 0 300px;
   width: 300px;
   background-color: #30333A;
   overflow: auto;
+  animation: ${pushRight} .2s linear;
 `
