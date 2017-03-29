ff48bd4c32c145ab050d7cb178235ac47ab33b41
Irfan Nuri Karaca <irfannuri@gmail.com>
2017-03-29 15:10:45 +0100

    Jump to next field or submit if last on connect & change password frames

--- src/browser/modules/Stream/Auth/ConnectForm.jsx
+++ src/browser/modules/Stream/Auth/ConnectForm.jsx
@@ -6,25 +6,29 @@ import {
   StyledConnectionLabel,
   StyledConnectionFormEntry
 } from './styled'
+import FormKeyHandler from 'browser-components/form/formKeyHandler'
 
 export default class ConnectForm extends Component {
+  componentWillMount () {
+    this.formKeyHandler = new FormKeyHandler(this.props.onConnectClick)
+  }
   componentDidMount () {
-    this.firstInput.focus()
+    this.formKeyHandler.initialize()
   }
   render () {
     return (
       <StyledConnectionForm>
         <StyledConnectionFormEntry>
           <StyledConnectionLabel>Host</StyledConnectionLabel>
-          <StyledConnectionTextInput innerRef={(el) => (this.firstInput = el)} onChange={this.props.onHostChange} value={this.props.host} />
+          <StyledConnectionTextInput innerRef={(el) => this.formKeyHandler.registerInput(el, 1)} onChange={this.props.onHostChange} value={this.props.host} />
         </StyledConnectionFormEntry>
         <StyledConnectionFormEntry>
           <StyledConnectionLabel>Username</StyledConnectionLabel>
-          <StyledConnectionTextInput onChange={this.props.onUsernameChange} value={this.props.username} />
+          <StyledConnectionTextInput innerRef={(el) => this.formKeyHandler.registerInput(el, 2)} onChange={this.props.onUsernameChange} value={this.props.username} />
         </StyledConnectionFormEntry>
         <StyledConnectionFormEntry>
           <StyledConnectionLabel>Password</StyledConnectionLabel>
-          <StyledConnectionTextInput onChange={this.props.onPasswordChange} value={this.props.password} type='password' />
+          <StyledConnectionTextInput innerRef={(el) => this.formKeyHandler.registerInput(el, 3)} onChange={this.props.onPasswordChange} value={this.props.password} type='password' />
         </StyledConnectionFormEntry>
         <FormButton onClick={this.props.onConnectClick}>Connect</FormButton>
       </StyledConnectionForm>
