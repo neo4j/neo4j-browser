0f74b1e4371b9e601da5338a354a04547f8c1b36
Jens Oknelid <jens.oknelid@gmail.com>
2017-04-28 15:43:36 +0200

    Added animation when closing sidebar

--- src/browser/components/TabNavigation/Navigation.jsx
+++ src/browser/components/TabNavigation/Navigation.jsx
@@ -18,53 +18,112 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { Component } from 'preact'
 import { StyledNavigationButton, NavigationButtonContainer } from 'browser-components/buttons'
-import { StyledSidebar, StyledDrawer, HiddenDrawer, StyledTabsWrapper, StyledTopNav, StyledBottomNav } from './styled'
-const Navigation = ({
-  openDrawer,
-  onNavClick,
-  topNavItems,
-  bottomNavItems = [],
-  sidebarClassName
-}) => {
-  const buildNavList = (list, selected) => {
-    return list.map((item, index) => {
-      const isOpen = item.name.toLowerCase() === selected
-      return (
-        <NavigationButtonContainer
-          key={index}
-          onClick={() => onNavClick(item.name.toLowerCase())}
-          isOpen={isOpen}
-        >
-          <StyledNavigationButton name={item.name}>{item.icon(isOpen)}</StyledNavigationButton>
-        </NavigationButtonContainer>
-      )
+import { StyledSidebar, StyledDrawer, StyledTabsWrapper, StyledTopNav, StyledBottomNav } from './styled'
+
+const Closing = 'CLOSING'
+const Closed = 'CLOSED'
+const Open = 'OPEN'
+const Opening = 'OPENING'
+
+class Navigation extends Component {
+
+  constructor (props) {
+    super(props)
+    this._onTransitionEnd = this.onTransitionEnd.bind(this)
+    this.setState({
+      transitionState: Closed
     })
   }
-  const getContentToShow = (openDrawer) => {
-    if (openDrawer) {
-      let filteredList = topNavItems.concat(bottomNavItems).filter((item) => {
-        return item.name.toLowerCase() === openDrawer
-      })
-      let TabContent = filteredList[0].content
-      return <TabContent />
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.openDrawer !== this.props.openDrawer) {
+      var newState = {}
+      if (nextProps.openDrawer) {
+        newState.drawerContent = nextProps.openDrawer
+        if (this.state.transitionState === Closed || this.state.transitionState === Closing) {
+          newState.transitionState = Opening
+        }
+      } else {
+        if (this.state.transitionState === Open || this.state.transitionState === Opening) {
+          newState.transitionState = Closing
+        }
+      }
+      this.setState(newState)
     }
-    return null
   }
-  const topNavItemsList = buildNavList(topNavItems, openDrawer)
-  const bottomNavItemsList = buildNavList(bottomNavItems, openDrawer)
-  const DrawerComponent = openDrawer ? StyledDrawer : HiddenDrawer
-  return (
-    <StyledSidebar>
-      <StyledTabsWrapper>
-        <StyledTopNav>{topNavItemsList}</StyledTopNav>
-        <StyledBottomNav>{bottomNavItemsList}</StyledBottomNav>
-      </StyledTabsWrapper>
-      <DrawerComponent>
-        {getContentToShow(openDrawer)}
-      </DrawerComponent>
-    </StyledSidebar>
-  )
+
+  onTransitionEnd () {
+    if (this.transitionState === Closing) {
+      this.setState({
+        transitionState: Closed,
+        drawerContent: null
+      })
+    }
+    if (this.transitionState === Opening) {
+      this.setState({
+        transitionState: Open
+      })
+    }
+  }
+
+  render () {
+    let {
+      onNavClick,
+      topNavItems,
+      bottomNavItems = []
+    } = this.props
+
+    const buildNavList = (list, selected) => {
+      return list.map((item, index) => {
+        const isOpen = item.name.toLowerCase() === selected
+        return (
+          <NavigationButtonContainer
+            key={index}
+            onClick={() => onNavClick(item.name.toLowerCase())}
+            isOpen={isOpen}
+          >
+            <StyledNavigationButton name={item.name}>{item.icon(isOpen)}</StyledNavigationButton>
+          </NavigationButtonContainer>
+        )
+      })
+    }
+    const getContentToShow = (openDrawer) => {
+      if (openDrawer) {
+        let filteredList = topNavItems.concat(bottomNavItems).filter((item) => {
+          return item.name.toLowerCase() === openDrawer
+        })
+        let TabContent = filteredList[0].content
+        return <TabContent />
+      }
+      return null
+    }
+    const topNavItemsList = buildNavList(topNavItems, this.state.drawerContent)
+    const bottomNavItemsList = buildNavList(bottomNavItems, this.state.drawerContent)
+
+    return (
+      <StyledSidebar>
+        <StyledTabsWrapper>
+          <StyledTopNav>{topNavItemsList}</StyledTopNav>
+          <StyledBottomNav>{bottomNavItemsList}</StyledBottomNav>
+        </StyledTabsWrapper>
+        <StyledDrawer
+          open={this.state.transitionState === Open || this.state.transitionState === Opening}
+          innerRef={(ref) => {
+            if (ref) {
+              // Remove old listeners so we don't get multiple callbacks.
+              // This function is called more than once with same html element
+              ref.removeEventListener('transitionend', this._onTransitionEnd)
+              ref.addEventListener('transitionend', this._onTransitionEnd)
+            }
+          }}
+        >
+          {getContentToShow(this.state.drawerContent)}
+        </StyledDrawer>
+      </StyledSidebar>
+    )
+  }
 }
 
 export default Navigation
