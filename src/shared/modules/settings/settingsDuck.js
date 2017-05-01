34ed5be65f757e215c3d0a0903b03a1c36c8db11
Oskar Hane <oh@oskarhane.com>
2017-05-01 21:33:51 +0200

    Add test for shouldReportUdc

--- src/shared/modules/settings/settingsDuck.js
+++ src/shared/modules/settings/settingsDuck.js
@@ -37,7 +37,7 @@ export const getBrowserSyncConfig = (state) => {
 export const getMaxNeighbours = (state) => state[NAME].maxNeighbours || initialState.maxNeighbours
 export const getMaxRows = (state) => state[NAME].maxRows || initialState.maxRows
 export const getInitialNodeDisplay = (state) => state[NAME].initialNodeDisplay || initialState.initialNodeDisplay
-export const shouldReportUdc = (state) => state[NAME].shouldReportUdc || initialState.shouldReportUdc
+export const shouldReportUdc = (state) => state[NAME].shouldReportUdc !== false
 
 const browserSyncConfig = {
   authWindowUrl: 'https://auth.neo4j.com/indexNewBrowser.html',
