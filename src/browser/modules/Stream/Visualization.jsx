d4c78460b97e029459b8edc7ed60dc3f25a7f6ea
Oskar Hane <oh@oskarhane.com>
2017-05-04 19:57:07 +0200

    Catch any evetual errors from query

--- src/browser/modules/Stream/Visualization.jsx
+++ src/browser/modules/Stream/Visualization.jsx
@@ -83,6 +83,7 @@ export class Visualization extends Component {
         .then((graph) => {
           this.autoCompleteCallback && this.autoCompleteCallback(graph.relationships)
         })
+        .catch((e) => {})
     }
   }
 
