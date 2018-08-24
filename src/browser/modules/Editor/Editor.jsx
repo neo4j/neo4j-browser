800e3fcc719e80709362709c7c6776b3cbbba69e
Oskar Hane <oh@oskarhane.com>
2018-08-24 16:28:38 +0200

    Revert "Don’t assume ref being there on slow machines"
    This reverts commit c119ef4f25f2572060c19c97e19bff50759aaf89.

--- src/browser/modules/Editor/Editor.jsx
+++ src/browser/modules/Editor/Editor.jsx
@@ -172,7 +172,7 @@ export class Editor extends Component {
     this.setEditorValue(this.props.history[this.state.historyIndex])
   }
 
-  triggerAutocompletion = (cm, changed) => {
+  triggerAutocompletion (cm, changed) {
     if (changed.text.length !== 1 || !this.props.enableEditorAutocomplete) {
       return
     }
@@ -208,18 +208,10 @@ export class Editor extends Component {
   }
 
   componentDidMount () {
-    this.attachCodemirrorInstance()
+    this.codeMirror = this.editor.getCodeMirror()
+    this.codeMirror.on('change', this.triggerAutocompletion.bind(this))
   }
-  attachCodemirrorInstance = () => {
-    if (!this.codeMirror) {
-      setTimeout(() => {
-        if (this.editor) {
-          this.codeMirror = this.editor.getCodeMirror()
-          this.codeMirror.on('change', this.triggerAutocompletion)
-        }
-      }, 100)
-    }
-  }
+
   getEditorValue () {
     return this.codeMirror ? this.codeMirror.getValue().trim() : ''
   }
