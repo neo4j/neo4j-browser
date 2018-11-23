b62b31143d73782f2aced7825f7bd682a28c9703
Oskar Hane <oh@oskarhane.com>
2018-11-23 11:51:53 +0100

    Add more viz styling colors
    Be consistent with Neo4j Bloom

--- src/browser/modules/D3Visualization/graphStyle.js
+++ src/browser/modules/D3Visualization/graphStyle.js
@@ -111,39 +111,64 @@ export default function neoGraphStyle () {
   ]
   const defaultColors = [
     {
-      color: '#A5ABB6',
+      color: '#FFE081',
       'border-color': '#9AA1AC',
       'text-color-internal': '#FFFFFF'
     },
     {
-      color: '#68BDF6',
-      'border-color': '#5CA8DB',
+      color: '#C990C0',
+      'border-color': '#b261a5',
       'text-color-internal': '#FFFFFF'
     },
     {
-      color: '#6DCE9E',
-      'border-color': '#60B58B',
+      color: '#F79767',
+      'border-color': '#f36924',
       'text-color-internal': '#FFFFFF'
     },
     {
-      color: '#FF756E',
-      'border-color': '#E06760',
+      color: '#57C7E3',
+      'border-color': '#23b3d7',
       'text-color-internal': '#FFFFFF'
     },
     {
-      color: '#DE9BF9',
-      'border-color': '#BF85D6',
+      color: '#F16667',
+      'border-color': '#eb2728',
       'text-color-internal': '#FFFFFF'
     },
     {
-      color: '#FB95AF',
-      'border-color': '#E0849B',
-      'text-color-internal': '#FFFFFF'
-    },
-    {
-      color: '#FFD86E',
-      'border-color': '#EDBA39',
+      color: '#D9C8AE',
+      'border-color': '#c0a378',
       'text-color-internal': '#604A0E'
+    },
+    {
+      color: '#8DCC93',
+      'border-color': '#5db665',
+      'text-color-internal': '#604A0E'
+    },
+    {
+      color: '#ECB5C9',
+      'border-color': '#da7298',
+      'text-color-internal': '#604A0E'
+    },
+    {
+      color: '#4C8EDA',
+      'border-color': '#2870c2',
+      'text-color-internal': '#FFFFFF'
+    },
+    {
+      color: '#FFC454',
+      'border-color': '#d7a013',
+      'text-color-internal': '#604A0E'
+    },
+    {
+      color: '#DA7194',
+      'border-color': '#cc3c6c',
+      'text-color-internal': '#FFFFFF'
+    },
+    {
+      color: '#569480',
+      'border-color': '#447666',
+      'text-color-internal': '#FFFFFF'
     }
   ]
   const Selector = (function () {
