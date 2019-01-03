70aaef2e26a0f730dd435fe75ea44dfc587a3d0a
Oskar Hane <oh@oskarhane.com>
2019-01-03 13:38:14 +0100

    Update company in copyright headers
    Network Engine for Objects in Lund AB [http://neotechnology.com]
    to
    Neo4j Sweden AB [http://neo4j.com]

--- build_scripts/webpack-rules.js
+++ build_scripts/webpack-rules.js
@@ -1,6 +1,6 @@
 /*
-* Copyright (c) 2002-2019 "Neo4j, Inc"
-* Network Engine for Objects in Lund AB [http://neotechnology.com]
+* Copyright (c) 2002-2019 "Neo4j,"
+* Neo4j Sweden AB [http://neo4j.com]
 *
 * This file is part of Neo4j.
 *
