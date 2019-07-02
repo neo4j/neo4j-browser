9e89109971518724d74446aebda46061cb0c9c4c
Linus Lundahl <lundahl.linus@gmail.com>
2019-07-02 14:27:09 +0200

    Fix collapse up/down icon in titlebar.

--- src/browser/components/icons/Icons.jsx
+++ src/browser/components/icons/Icons.jsx
@@ -29,7 +29,6 @@ import cloudCheck from 'icons/cloud-check.svg'
 import cloudRemove from 'icons/cloud-remove.svg'
 import neo4j from 'icons/neo-world.svg'
 import pin from 'icons/pin.svg'
-import arrowUp1 from 'icons/arrow-up-1.svg'
 import close from 'icons/close.svg'
 import expand01 from 'icons/expand-01.svg'
 import shrink from 'icons/shrink.svg'
@@ -241,7 +240,7 @@ export const RefreshIcon = () => (
   <IconContainer icon={buttonRefreshArrow} width={12} />
 )
 export const CloseIcon = () => <IconContainer icon={close} width={12} />
-export const UpIcon = () => <IconContainer icon={arrowUp1} width={12} />
+export const UpIcon = () => <IconContainer className='sl-chevron-up' />
 export const DownIcon = () => <IconContainer className='sl-chevron-down' />
 export const DoubleUpIcon = () => <IconContainer className='sl-double-up' />
 export const DoubleDownIcon = () => <IconContainer className='sl-double-down' />
