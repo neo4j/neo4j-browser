8e0078d8d2602399538e966830ba00794540d7f9
Linus Lundahl <lundahl.linus@gmail.com>
2019-09-10 11:52:34 +0200

    Update help for params from Hugo.

--- src/browser/documentation/help/params.jsx
+++ src/browser/documentation/help/params.jsx
@@ -25,28 +25,50 @@ const category = 'cypherQueries'
 const content = (
   <React.Fragment>
     <p>
-      The <code>:params</code> command will show you a list of all your current
-      parameters.
+      The
+      <code>:param name => 'Stella'</code> command will define a parameter named
+      "name" and it will be sent along with your queries.
+      <br /> Using parameters, rather than hard coding values, will allow for
+      reuse of the query plan cache
     </p>
     <p>
-      Note that setting parameters using this method does not provide type
-      safety with numbers.
+      The right hand side of
+      <code>=></code> is sent to the server and evaluated as Cypher with an
+      implicit
+      <code>RETURN</code> in front. This gives better type safety since some
+      types (especially numbers) in JavaScript are hard to match with Neo4j:s
+      type system.
       <br />
-      Instead we advise you to set each param one by one using the{' '}
-      <code>:param x => 1</code> syntax.
-      <br />
-      See <a help-topic='param'>:help param</a> for more info.
+      To set a param as an integer, do
+      <code>:param x => 1</code> and to set it as a float, do
+      <code>:param x => 1.0</code>.
     </p>
     <p>
-      The <code>:params {`{name: 'Stella', age: 24}`}</code> command will
-      replace your current parameters with the new parameters defined in the
-      object.
+      If you need more fine-grained control or advanced Cypher queries, you can
+      use the explicit syntax: <code>{`x => { ... RETURN 1 as foo }`}</code>
+      <br />
+      Explicit returns yield a list of records, matching that of your Cypher
+      query: <code>{`x => { RETURN 1 as foo }`}</code> yields{' '}
+      <code>{`$x = [{foo: 1}]`}</code>
+      <br />
+      You can pick out individual values from your result using destructuring:{' '}
+      <code>{`[{foo}] => { RETURN 1 as foo }`}</code> yields{' '}
+      <code>{`$foo = 1`}</code>
+      <br />
+      You can also rename destructured params:{' '}
+      <code>{`[{foo: bar}] => { RETURN 1 as foo }`}</code> yields{' '}
+      <code>{`$bar = 1`}</code>
+    </p>
+    <p>
+      Cypher query example with a param:
+      {` `}
+      <code>{`MATCH (n:Person) WHERE n.name = $name`}</code>
     </p>
     <div className='links'>
       <div className='link'>
         <p className='title'>Related</p>
         <p className='content'>
-          <a help-topic='param'>:help param</a>
+          <a help-topic='params'>:help params</a>
         </p>
       </div>
     </div>
