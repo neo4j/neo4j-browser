b6e377fb11d66595d263c73b4262b0621312ac23
Oskar Hane <oh@oskarhane.com>
2020-01-07 17:37:14 +0100

    🎇😱

--- src/browser/modules/Sidebar/About.jsx
+++ src/browser/modules/Sidebar/About.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
@@ -45,7 +45,7 @@ const About = ({ serverVersion, serverEdition }) => (
         </DrawerSubHeader>
       </DrawerSection>
       <DrawerSection>
-        <DrawerSectionBody>Copyright &#169; 2002-2019</DrawerSectionBody>
+        <DrawerSectionBody>Copyright &#169; 2002-2020</DrawerSectionBody>
       </DrawerSection>
       <DrawerSection>
         <DrawerSubHeader>You are running</DrawerSubHeader>
