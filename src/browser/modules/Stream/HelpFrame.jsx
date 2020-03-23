63eae8244643b523cd6a390a5b1759a918a305d2
Oskar Hane <oh@oskarhane.com>
2020-03-23 10:45:12 +0100

    Add prev / next buttons to help frame

--- src/browser/modules/Stream/HelpFrame.jsx
+++ src/browser/modules/Stream/HelpFrame.jsx
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Docs from '../Docs/Docs'
 import docs from '../../documentation'
 import Directives from 'browser-components/Directives'
@@ -25,39 +25,62 @@ import FrameTemplate from '../Frame/FrameTemplate'
 import FrameAside from '../Frame/FrameAside'
 import { transformCommandToHelpTopic } from 'services/commandUtils'
 import { DynamicTopics } from '../../documentation/templates/DynamicTopics'
-import Carousel from '../Carousel/Carousel'
-import Slide from '../Carousel/Slide'
+import { CarouselButton } from 'browser-components/buttons/index'
+import {
+  SlideNextIcon,
+  SlidePreviousIcon
+} from 'browser-components/icons/Icons'
 
 const HelpFrame = ({ frame, stack = [] }) => {
-  const { aside, main } = generateContent(frame)
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(0)
+  const currentFrame = stack[currentFrameIndex]
 
-  console.log('stack: ', stack)
+  // When we get a new frame, go to it
+  useEffect(() => {
+    setCurrentFrameIndex(0)
+  }, [stack.length])
+
+  const { aside, main } = generateContent(currentFrame)
+
+  const prevBtn = (
+    <CarouselButton
+      className="previous-slide  rounded"
+      data-testid="previousSlide"
+      disabled={currentFrameIndex === stack.length - 1}
+      onClick={() => setCurrentFrameIndex(currentFrameIndex + 1)}
+    >
+      <SlidePreviousIcon />
+    </CarouselButton>
+  )
+
+  const nextBtn = (
+    <CarouselButton
+      className="next-slide rounded"
+      data-testid="nextSlide"
+      disabled={currentFrameIndex === 0}
+      onClick={() => setCurrentFrameIndex(currentFrameIndex - 1)}
+    >
+      <SlideNextIcon />
+    </CarouselButton>
+  )
+
   const contents =
     stack.length > 1 ? (
-      <Carousel
-        className="deck container-fluid"
-        slides={stack.map(generateDirective).reverse()}
-        initialSlide={stack.length}
-      />
+      <React.Fragment>
+        {prevBtn}
+        <Directives originFrameId={frame.id} content={main} />
+        {nextBtn}
+      </React.Fragment>
     ) : (
       <Directives originFrameId={frame.id} content={main} />
     )
   return (
     <FrameTemplate
-      className="helpFrame has-carousel"
-      header={frame}
+      className="helpFrame has-stack"
+      header={currentFrame}
       aside={aside}
       contents={contents}
     />
-  )
-}
-
-function generateDirective(frame, i) {
-  const { aside, main } = generateContent(frame)
-  return (
-    <Slide key={`${frame.id}-${i}`}>
-      <Directives originFrameId={frame.id} content={main} />
-    </Slide>
   )
 }
 
