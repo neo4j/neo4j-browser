9563810ac6f8c405fa9b20ec37a928f25b372034
Oskar Damkjaer <OskarDamkjaer@users.noreply.github.com>
2020-08-04 15:31:18 +0200

    Clarify why webpack is needed in cypress

--- e2e_tests/plugins/index.js
+++ e2e_tests/plugins/index.js
@@ -2,6 +2,8 @@ const webpack = require('@cypress/webpack-preprocessor')
 
 module.exports = on => {
   const config = require('../../build_scripts/webpack.config.js')
+  // To be able to import our redux actions we need to 
+  // use webpack (handles typescript/aliased imports/etc)
   // We want to use the same config when running the browser normally
   // however the settings for optimzations break cypress for some reason
   delete config.optimization
