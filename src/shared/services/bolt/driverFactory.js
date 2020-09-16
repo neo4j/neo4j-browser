07f138866b4509b1564fd76f734d9ad61b0d5727
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2020-09-16 14:35:26 +0200

    Add useragent when to neo4j-driver config

--- src/shared/services/bolt/driverFactory.js
+++ src/shared/services/bolt/driverFactory.js
@@ -19,11 +19,12 @@
  */
 
 import neo4j from 'neo4j-driver'
+import { version } from 'project-root/package.json'
 
 export const createDriverOrFailFn = (url, auth, opts, failFn = () => {}) => {
   // This is needed, I haven't figured out why. I don't find any mutations to
   // the object, so not sure what's going on.
-  const spreadOpts = { ...opts }
+  const spreadOpts = { ...opts, userAgent: `neo4j-browser/v${version}` }
   try {
     const res = neo4j.driver(url, auth, spreadOpts)
     return res
