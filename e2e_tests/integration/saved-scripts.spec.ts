a3f12ce00e08b7815c6b92836c5490264e4e8dc1
Emil Andersson <emil.andersson@neotechnology.com>
2021-01-20 11:36:44 +0100

    update select all and delete command to work on linux

--- e2e_tests/integration/saved-scripts.spec.ts
+++ e2e_tests/integration/saved-scripts.spec.ts
@@ -41,8 +41,10 @@ describe('Saved Scripts', () => {
     cy.get('[data-testid="scriptTitle-script name"]').click()
     cy.get('[data-testid="currentlyEditing"]').contains('script name')
     // Editing script updates name and content
+    const clearInput =
+      Cypress.platform == 'darwin' ? '{cmd}a{del}' : '{ctrl}a{del}'
     cy.get('[data-testid="activeEditor"] textarea')
-      .type('{meta}a{del}') // select all and delete
+      .type(clearInput)
       .type('// Guide{shift}{enter}:play movies', { force: true })
     cy.get('[title="Update favorite"]').click()
 
