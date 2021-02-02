651b92d28d6cbe244ff00f996e850bc2e6b1b79a
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-02-02 10:39:52 +0100

    Flaky-ness fix

--- e2e_tests/support/commands.ts
+++ e2e_tests/support/commands.ts
@@ -100,7 +100,7 @@ Cypress.Commands.add('disconnect', () => {
 })
 
 Cypress.Commands.add('typeInFrame', (cmd: string, frameIndex = 0) => {
-  cy.get('[id^=monaco-]')
+  cy.get('[id^=monaco-] textarea')
     .should('have.length.at.least', frameIndex + 1)
     .eq(frameIndex + 1) // the first monaco editor is the main one
     .type(
