2f2a641976937e9ab44abed2bdc0a5dae21ab6b4
Emil Andersson <emil.andersson@neotechnology.com>
2021-03-01 09:45:30 +0100

    remove peek problems status bar

--- src/browser/modules/Editor/Monaco.tsx
+++ src/browser/modules/Editor/Monaco.tsx
@@ -67,6 +67,11 @@ const MonacoStyleWrapper = styled.div`
   .margin .margin-view-overlays {
     margin-left: 10px;
   }
+
+  // hides the "Peek Problem" status bar on the warnings hover widgets
+  .hover-row.status-bar {
+    display: none !important;
+  }
 `
 
 interface MonacoProps {
