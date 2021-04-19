39a2b06d2e040908db129298fe7b342ff4f73574
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-04-19 16:44:01 +0200

    incorrect link

--- src/browser/documentation/sidebar-guides/intro.tsx
+++ src/browser/documentation/sidebar-guides/intro.tsx
@@ -170,8 +170,7 @@ const slides = [
     <h3>Jump into code</h3>
     <ul className="undecorated">
       <li>
-        <a data-exec=":guide northwind-graph">Cypher Guide</a> - Learn Cypher
-        basics
+        <a data-exec=":guide cypher">Cypher Guide</a> - Learn Cypher basics
       </li>
     </ul>
   </SidebarSlide>
