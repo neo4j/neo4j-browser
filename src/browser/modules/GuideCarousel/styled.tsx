dcb89bf31f5056107d98107152d113c1b407abbd
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-06-14 14:24:41 +0200

    Show current index more clearly

--- src/browser/modules/GuideCarousel/styled.tsx
+++ src/browser/modules/GuideCarousel/styled.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const PaginationItem = styled.span<{ active: boolean }>`
   padding: 0 3px;
-  ${props => (props.active ? 'color: white;' : null)}
+  color: ${props => (props.active ? 'white' : 'inherit')};
+  text-decoration: ${props => (props.active ? 'underline' : 'none')};
   cursor: pointer;
 `
