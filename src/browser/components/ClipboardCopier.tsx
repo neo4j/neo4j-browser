5b783ae7ee63bf055b0d5a5c75402883dd09ccfb
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-06-24 17:01:57 +0200

    Change text as per lizas comment

--- src/browser/components/ClipboardCopier.tsx
+++ src/browser/components/ClipboardCopier.tsx
@@ -24,7 +24,7 @@ function ClipboardCopier({
       <CopyIconContainer
         onClick={() =>
           copyToClipboard(text)
-            .then(() => showPopup('Copied.'))
+            .then(() => showPopup('Copied'))
             .catch(() => showPopup('Copying text failed.'))
         }
       >
