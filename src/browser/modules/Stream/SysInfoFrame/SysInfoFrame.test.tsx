f3a0ee292b62b315ebd0f3042b9f864a12588a43
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-07-08 14:00:05 +0200

    Rename sysinfo frame to be researchable

--- src/browser/modules/Stream/SysInfoFrame/SysInfoFrame.test.tsx
+++ src/browser/modules/Stream/SysInfoFrame/SysInfoFrame.test.tsx
@@ -20,7 +20,7 @@
 
 import React from 'react'
 import { render } from '@testing-library/react'
-import { SysInfoFrame } from './index'
+import { SysInfoFrame } from './SysInfoFrame'
 
 jest.mock(
   'browser/modules/Frame/FrameTemplate',
