5c595263200e204e96caf00e54c4dea37a815bc4
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-08-16 13:28:59 +0200

    Fix test

--- e2e_tests/integration/data-export.spec.ts
+++ e2e_tests/integration/data-export.spec.ts
@@ -80,7 +80,14 @@ describe('Data export', () => {
   it('can export history', () => {
     cy.executeCommand(':clear')
     cy.executeCommand(':history')
-    cy.get('[data-testid="exportGrassButton"]').should('not.exist')
-    cy.get('[data-testid="exportHistoryButton"]').should('not.exist')
+    cy.get('[data-testid="frame-export-dropdown"]').trigger('mouseover')
+    cy.get('[data-testid="frame-export-dropdown"]', {
+      timeout: 10000
+    }).within(() => {
+      cy.get('a').then(exportButtonsList => {
+        expect(exportButtonsList).to.have.length(1)
+        expect(exportButtonsList.eq(0)).to.contain('Export TXT')
+      })
+    })
   })
 })
