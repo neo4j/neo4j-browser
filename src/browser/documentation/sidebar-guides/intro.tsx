ebbba786f1233c565fcfdfe7a289daacc0cbe154
Reneta Popova <reneta.popova@neo4j.com>
2021-09-13 17:06:13 +0100

    updated the sidebar intro guide and removed the intro guide from the play-guide folder

--- src/browser/documentation/sidebar-guides/intro.tsx
+++ src/browser/documentation/sidebar-guides/intro.tsx
@@ -22,31 +22,34 @@ import React from 'react'
 import { BuiltInGuideSidebarSlide } from '../../modules/Carousel/Slide'
 import { isMac } from 'browser/modules/App/keyboardShortcuts'
 
-const title = 'Introduction'
+const title = 'Intro Guide'
 const slides = [
   <BuiltInGuideSidebarSlide key="s1">
+    <h3>Neo4j Browser</h3>
     <p>
-      Neo4j Browser is a command driven client, like a web-based shell
-      environment. It is perfect for running ad-hoc graph queries, with just
-      enough ability to prototype a Neo4j-based application.
+      Neo4j Browser is a command-driven client as a web-based shell environment.
+      It is perfect for running ad-hoc graph queries, with just enough ability
+      to prototype a Neo4j-based application.
     </p>
     <ul className="big">
+      <li>Developer focused.</li>
+      <li>Used for writing and running Cypher graph queries.</li>
+      <li>Exportable tabular results of any query result.</li>
       <li>
-        Developer focused, for writing and running graph queries with Cypher
+        Graph visualization of query results containing nodes and relationships.
       </li>
-      <li>Exportable tabular results of any query result</li>
-      <li>
-        Graph visualization of query results containing nodes and relationships
-      </li>
+      <li>Convenient exploration of the Neo4j's REST API.</li>
     </ul>
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s2">
-    <h3>Editor</h3>
-    <p className="lead">Command editing and execution</p>
+    <h3>Browser Editor (a.k.a. Editor)</h3>
+    <p className="lead">
+      <em>Edit and execute Cypher statements and Browser commands</em>
+    </p>
     <p>
-      Editor pane is used to edit and run for Cypher statements and Browser
-      commands. Browser commands begin with <code>:</code>, for example{' '}
-      <code>:help</code>
+      Editor is the primary interface for entering and running Cypher statements
+      and Browser commands. Browser commands begin with <code>:</code>, for
+      example, <code>:help</code>.
     </p>
     <table>
       <tbody>
@@ -79,35 +82,36 @@ const slides = [
     <p></p>
     <p>
       You can view the list of keybinding anytime by running{' '}
-      <code>:help keys</code> or by pressing <b>F1 </b>in the editor to see all
-      editor-specific keybindings.
+      <code>:help keys</code> or by pressing <b>F1 </b>in the Editor to see all
+      Editor-specific keybindings.
     </p>
     <br />
     <video autoPlay loop muted playsInline>
-      <source src="./assets/images/Keystores.mp4" type="video/mp4" />
+      <source src="./assets/images/Keystrokes.mp4" type="video/mp4" />
     </video>
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s3">
     <h3>Result frame</h3>
-    <p className="lead">Most recently executed command or Cypher query.</p>
+    <p className="lead">
+      <em>Most recently executed command or Cypher query</em>
+    </p>
     <p>
-      A result frame is created for each command execution, added to the top of
-      the stream to create a scrollable collection in reverse chronological
-      order.
+      A result frame is created for each execution and added to the top of the
+      stream to create a scrollable collection in reverse chronological order.
     </p>
     <ul>
-      <li>If a frame is pinned it will always stay in the same position. </li>
+      <li>A pinned frame always stays in the same position. </li>
       <li>
-        You can clear the stream of result frames running <code>:clear</code>{' '}
-        command.
+        You can clear the stream of result frames by running the{' '}
+        <code>:clear</code> command.
       </li>
       <li>
-        There are maximum 30 result frames displayed in the stream. You can
-        change this number in the Settings sidebar.{' '}
+        The maximum number of result frames displayed in the stream is 30. You
+        can change this number in the Browser Settings drawer.{' '}
       </li>
       <li>
         You can bring up the history of the executed commands and queries by
-        running <code className="nobreak">:history</code> command.
+        running the <code className="nobreak">:history</code> command.
       </li>
     </ul>
     <br />
@@ -115,24 +119,34 @@ const slides = [
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s4">
     <h3>Reusable frame</h3>
-    <p className="lead">Instead of the stream</p>
+    <p className="lead">
+      <em>Ammend your query in place</em>
+    </p>
     <p>
       You can also iterate in the same frame instead of generating a scrollable
       stream of frames.
     </p>
-    <p>Adjust your preferences in the Settings sidebar tab.</p>
+    <p>
+      Each reusable frame maintains its own local history of commands and
+      updates the main one in the Editor, should you need to instantiate a new
+      result frame from there.
+    </p>
     <br />
     <video autoPlay loop muted playsInline>
       <source src="./assets/images/ReusableFrame.mp4" type="video/mp4" />
     </video>
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s5">
-    <h3>Sidebar: Database information</h3>
-    When Neo4j is installed, it is initiated with two databases - a{' '}
+    <h3>Sidebar: Database</h3>
+    <p className="lead">
+      <em>Database infromation</em>
+    </p>
+    When Neo4j is installed, it is initiated with two databases – a{' '}
     <code>system</code> database and a default <code>neo4j</code> database.
-    Launching Neo4j Browser will automatically point us to the{' '}
-    <code className="nobreak">neo4j default</code> database, shown by the neo4j$
-    prompt in the editor.
+    Launching Neo4j Browser automatically points you to the{' '}
+    <code className="nobreak">neo4j</code> database, shown by the{' '}
+    <code>neo4j$</code>
+    prompt in the Editor.
     <br />
     <img
       src="./assets/images/SidebarDB_Iinfo.png"
@@ -141,12 +155,15 @@ const slides = [
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s6">
     <h3>Sidebar: Favorites</h3>
+    <p className="lead">
+      <em>Quick way to save your queries</em>
+    </p>
     <p>
       Favorite queries or commands can be saved in the local storage and
       displayed in the sidebar.{' '}
     </p>
     <p>
-      Favorites are global and independent of project or database which means
+      Favorites are global and independent of project or database, which means
       that you can access your Favorites from Neo4j Browser with different
       databases, hosting platforms, and data sets.
     </p>
@@ -157,10 +174,18 @@ const slides = [
   </BuiltInGuideSidebarSlide>,
   <BuiltInGuideSidebarSlide key="s7">
     <h3>Sidebar: Project files</h3>
-    <p className="lead">Save cypher files to share with your colleagues</p>
-    Queries and commands can also be saved as <b>Project files</b>. Project
-    folder can be reached through Neo4j Desktop and your hard disk. Note this
-    feature is only available when Neo4j Browser is run in Desktop.
+    <p className="lead">
+      <em>Save cypher files to share with your colleagues</em>
+    </p>
+    <p>
+      Project files allows you to save queries, guides, and other scripts, as
+      Cypher files. Unlike favorites, which are saved in your local browser
+      storage, project files are project-specific and are actual files stored in
+      the project directory on your hard drive. All saved files are listed under
+      the project they refer to.
+    </p>
+    <p></p>
+    <p>The Project Files drawer is Neo4j Desktop specific.</p>
     <br />
     <video autoPlay loop muted playsInline>
       <source src="./assets/images/ProjectFiles.mp4" type="video/mp4" />
@@ -170,18 +195,22 @@ const slides = [
     <h3>Next steps</h3>
     <ul className="undecorated">
       <li>
+        <a data-exec="guide concepts">Concepts Guide</a> - Learn about Neo4j
+        property graphs
+      </li>
+      <li>
+        <a data-exec="guide cypher">Cypher Guide</a> - Learn Cypher basics
+      </li>
+    </ul>
+    <br />
+    <h3>References</h3>
+    <ul className="undecorated">
+      <li>
         <a help-topic="commands">Help commands</a> - Useful Neo4j Browser
         commands
       </li>
       <li>
         <a help-topic="keys">Help keys</a> - Keyboard shortcuts
-      </li>
-    </ul>
-    <br />
-    <h3>Jump into code</h3>
-    <ul className="undecorated">
-      <li>
-        <a data-exec="guide cypher">Cypher Guide</a> - Learn Cypher basics
       </li>
     </ul>
   </BuiltInGuideSidebarSlide>
