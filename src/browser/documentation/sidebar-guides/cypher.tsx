86d9298d9125d1cea119000740c64cff70716771
Reneta Popova <reneta.popova@neo4j.com>
2021-09-17 09:30:44 +0100

    updated the cypher guide and removed the cyper play guide

--- src/browser/documentation/sidebar-guides/cypher.tsx
+++ src/browser/documentation/sidebar-guides/cypher.tsx
@@ -22,73 +22,104 @@ import React from 'react'
 import ManualLink from 'browser-components/ManualLink'
 import { BuiltInGuideSidebarSlide } from '../../modules/Carousel/Slide'
 
-const title = 'Cypher'
+const title = 'Cypher Guide '
 const slides = [
-  <BuiltInGuideSidebarSlide key="first">
-    <p className="lead">Neo4j's graph query language</p>
-    <p>Neo4j's Cypher language is purpose built for working with graph data.</p>
+  <BuiltInGuideSidebarSlide key="s1">
+    <p className="lead">
+      <em>Neo4j's graph query language</em>
+    </p>
+    <p>Neo4j's Cypher language is purpose-built for working with graph data.</p>
     <ul className="big">
-      <li>uses patterns to describe graph data</li>
-      <li>familiar SQL-like clauses</li>
-      <li>declarative, describing what to find, not how to find it</li>
+      <li>Uses patterns to describe graph data.</li>
+      <li>Familiar SQL-like clauses.</li>
+      <li>Declarative, describing what to find, not how to find it.</li>
     </ul>
   </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="second">
+  <BuiltInGuideSidebarSlide key="s2">
     <h3>CREATE</h3>
-    <p className="lead">Create a node</p>
+    <p className="lead">
+      <em>Create a node</em>
+    </p>
     <p>Let's use Cypher to generate a small social graph.</p>
-    <pre className="pre-scrollable code runnable">
-      {'CREATE (ee:Person { name: "Emil", from: "Sweden", klout: 99 })'}
-    </pre>
-    <ul>
-      <li>
-        <code>CREATE</code> clause to create data
-      </li>
-      <li>
-        <code>()</code> parenthesis to indicate a node
-      </li>
-      <li>
-        <code>ee:Person</code> a variable 'ee' and label 'Person' for the new
-        node
-      </li>
-      <li>
-        <code>{'{}'}</code> brackets to add properties to the node
-      </li>
-    </ul>
-  </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="third">
-    <h3>MATCH</h3>
-    <p className="lead">Finding nodes</p>
-    <p>Now find the node representing Emil:</p>
-    <pre className="pre-scrollable code runnable">
-      MATCH (ee:Person) WHERE ee.name = "Emil" RETURN ee;
-    </pre>
-    <ul>
-      <li>
-        <code>MATCH</code> clause to specify a pattern of nodes and
-        relationships
-      </li>
-      <li>
-        <code>(ee:Person)</code> a single node pattern with label 'Person' which
-        will assign matches to the variable 'ee'
-      </li>
-      <li>
-        <code>WHERE</code> clause to constrain the results
-      </li>
-      <li>
-        <code>ee.name = "Emil"</code> compares name property to the value "Emil"
-      </li>
-      <li>
-        <code>RETURN</code> clause used to request particular results
-      </li>
-    </ul>
-  </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="forth">
-    <h3>CREATE more</h3>
-    <p className="lead">Nodes and relationships</p>
     <p>
-      <code>CREATE</code>
-      clauses can create many nodes and relationships at once.
+      <b>NOTE:</b> This guide assumes that you use an empty graph.{' '}
+    </p>
+    <ol>
+      <li>
+        Click this code block and bring it into the Editor:
+        <pre className="pre-scrollable code runnable">
+          {
+            'CREATE (ee:Person { name: "Emil", from: "Sweden", kloutScore: 99 })'
+          }
+        </pre>
+        <ul>
+          <li>
+            <code>CREATE</code> creates the node.
+          </li>
+          <li>
+            <code>()</code> indicates the node.
+          </li>
+          <li>
+            <code>ee:Person</code> – <code>ee</code> is the node variable and{' '}
+            <code>Person</code> is the node label.
+          </li>
+          <li>
+            <code>{'{}'}</code> contains the properties that describe the node.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Run the Cypher code by clicking the <b>Run</b> button.
+      </li>
+    </ol>
+  </BuiltInGuideSidebarSlide>,
+  <BuiltInGuideSidebarSlide key="s3">
+    <h3>MATCH</h3>
+    <p className="lead">
+      <em>Find nodes</em>
+    </p>
+    <p>Now, find the node representing Emil.</p>
+    <ol>
+      <li>
+        Click this code block and bring it into the Editor:
+        <pre className="pre-scrollable code runnable">
+          MATCH (ee:Person) WHERE ee.name = "Emil" RETURN ee;
+        </pre>
+        <ul>
+          <li>
+            <code>MATCH</code> specifies a pattern of nodes and relationships.
+          </li>
+          <li>
+            <code>(ee:Person)</code> is a single node pattern with label{' '}
+            <code>Person</code>. It assigns matches to the variable{' '}
+            <code>ee</code>.
+          </li>
+          <li>
+            <code>WHERE</code> filters the query.
+          </li>
+          <li>
+            <code>ee.name = "Emil"</code> compares name property to the value{' '}
+            <code>Emil</code>.
+          </li>
+          <li>
+            <code>RETURN</code> returns particular results.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Run the Cypher code by clicking the <b>Run</b> button.
+      </li>
+      <br />
+    </ol>
+  </BuiltInGuideSidebarSlide>,
+  <BuiltInGuideSidebarSlide key="s4">
+    <h3>CREATE more data</h3>
+    <p className="lead">
+      <em>Nodes and relationships</em>
+    </p>
+    <p>
+      The <code>CREATE</code> clause can create many nodes and relationships at
+      once.
     </p>
     <pre className="pre-scrollable code runnable">
       {`MATCH (ee:Person) WHERE ee.name = "Emil"
@@ -101,10 +132,14 @@ CREATE (js:Person { name: "Johan", from: "Sweden", learn: "surfing" }),
 (ir)-[:KNOWS]->(js),(ir)-[:KNOWS]->(ally),
 (rvb)-[:KNOWS]->(ally)`}
     </pre>
+    <br />
   </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="fifth">
-    <h3>Pattern matching</h3>
-    <p className="lead">Describe what to find in the graph</p>
+
+  <BuiltInGuideSidebarSlide key="s5">
+    <h3>MATCH patterns</h3>
+    <p className="lead">
+      <em>Describe what to find in the graph</em>
+    </p>
     <p className="summary">
       For instance, a pattern can be used to find Emil's friends:
     </p>
@@ -114,94 +149,37 @@ WHERE ee.name = "Emil" RETURN ee, friends`}
     </pre>
     <ul>
       <li>
-        <code>MATCH</code>
-        clause to describe the pattern from known Nodes to found Nodes
+        <code>MATCH</code> describes what nodes will be retrieved based upon the
+        pattern.
       </li>
       <li>
-        <code>(ee)</code>
-        starts the pattern with a Person (qualified by WHERE)
+        <code>(ee)</code> is the anchor node that will be found based upon the{' '}
+        <code>WHERE</code> clause.
       </li>
       <li>
-        <code>-[:KNOWS]-</code>
-        matches "KNOWS" relationships (in either direction)
+        <code>-[:KNOWS]-</code> matches <code>KNOWS</code> relationships (in
+        either direction) from <code>ee</code>.
       </li>
       <li>
-        <code>(friends)</code>
-        will be bound to Emil's friends
+        <code>(friends)</code> represents the nodes that are Emil's friends.
+      </li>
+      <li>
+        <code>RETURN</code> returns the anchor node <code>(ee)</code> and the
+        related <code>(friends)</code> nodes found.
       </li>
     </ul>
   </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="sixth">
-    <h3>Recommend</h3>
-    <p className="lead">Using patterns</p>
-    <p className="summary">
-      Pattern matching can be used to make recommendations. Johan is learning to
-      surf, so he may want to find a new friend who already does:
-    </p>
-    <pre className="pre-scrollable code runnable">
-      {`MATCH (js:Person)-[:KNOWS]-()-[:KNOWS]-(surfer)
-WHERE js.name = "Johan" AND surfer.hobby = "surfing"
-RETURN DISTINCT surfer`}
-    </pre>
-    <ul>
-      <li>
-        <code>()</code>
-        empty parenthesis to ignore these nodes
-      </li>
-      <li>
-        <code>DISTINCT</code>
-        because more than one path will match the pattern
-      </li>
-      <li>
-        <code>surfer</code>
-        will contain Allison, a friend of a friend who surfs
-      </li>
-    </ul>
-  </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="seventh">
-    <h3>Analyze</h3>
-    <p className="lead">Using the visual query plan</p>
-    <p className="summary">
-      Understand how your query works by prepending <code>EXPLAIN</code> or{' '}
-      <code>PROFILE</code>:
-    </p>
-    <pre className="pre-scrollable code runnable">
-      {`PROFILE MATCH (js:Person)-[:KNOWS]-()-[:KNOWS]-(surfer)
-WHERE js.name = "Johan" AND surfer.hobby = "surfing"
-RETURN DISTINCT surfer`}
-    </pre>
-  </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="eighth">
-    <h3>Live Cypher warnings</h3>
-    <p className="lead">Identify query problems in real time</p>
-    <p>
-      As you type, the query editor notifies you about deprecated features and
-      potentially expensive queries.
-    </p>
-    <img
-      src="./assets/images/screen_cypher_warn.png"
-      className="img-responsive"
-    />
-  </BuiltInGuideSidebarSlide>,
-  <BuiltInGuideSidebarSlide key="nineth">
+
+  <BuiltInGuideSidebarSlide key="s6">
     <h3>Next steps</h3>
-    <p>
-      Start your application using Cypher to create and query graph data. See{' '}
-      <a help-topic="cypher">Help Cypher</a> for more built-in cypher
-      documentation.
-    </p>
-    <h3>Documentation</h3>
     <ul className="undecorated">
       <li>
-        <ManualLink chapter="cypher-refcard" page="/">
-          Cypher Refcard
-        </ManualLink>
+        <a data-exec="guide movie-graph">The Movie Graph</a> – Queries and
+        recommendations with Cypher - movie use case.
       </li>
       <li>
-        <ManualLink chapter="cypher-manual" page="/">
-          The Cypher chapter
-        </ManualLink>{' '}
-        of the Neo4j Developer Manual
+        <a data-exec="guide northwind-graph">The Northwind Graph</a> – Translate
+        and import relation data into graph.
       </li>
     </ul>
   </BuiltInGuideSidebarSlide>
