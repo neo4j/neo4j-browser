b8dee90d6cc757774cc2b4960dc725b3d6d952f9
Junxiang Chen <chenfrey920417@outlook.com>
2021-11-18 22:55:07 +0000

    Init commit: introduce CircularDependencyPlugin

--- build_scripts/webpack-plugins.js
+++ build_scripts/webpack-plugins.js
@@ -32,6 +32,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const manifestGeneration = require('./generate-manifest-helpers')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = () => {
   const plugins = [
@@ -130,6 +131,17 @@ module.exports = () => {
       ],
       languages: [],
       filename: '[name]-[hash].worker.js'
+    }),
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      failOnError: true,
+      // allow import cycles that include an asyncronous import,
+      // e.g. via import(/* webpackMode: "weak" */ './file.js')
+      allowAsyncCycles: false,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd()
     })
   ]
 
