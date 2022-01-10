50efeaa21aab897fa02f500da56a4e646690d291
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2022-01-10 16:38:52 +0100

    Update tests

--- e2e_tests/integration/auto-prefix.spec.ts
+++ e2e_tests/integration/auto-prefix.spec.ts
@@ -37,7 +37,7 @@ describe(':auto prefix in browser', () => {
   it('adding :auto enables running periodic commit', () => {
     cy.executeCommand(':auto USING PERIODIC COMMIT RETURN "Laverre";')
     cy.getFrames().contains('ERROR')
-    cy.getFrames().contains('expected "LOAD"')
+    cy.getFrames().contains('LOAD')
   })
 
   if (Cypress.config('serverVersion') >= 4.4) {
