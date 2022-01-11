978b3807f4a765237e386a0ee6dd11f06d75c3e4
Angelo Gazzola <angelo@nglgzz.com>
2022-01-11 18:42:22 +0100

    Upgrade query plan view to D3 v7

--- src/browser/modules/D3Visualization/lib/visualization/utils/arrays.ts
+++ src/browser/modules/D3Visualization/lib/visualization/utils/arrays.ts
@@ -26,3 +26,50 @@ export default function cloneArray<T>(original: Array<T>): Array<T> {
   }
   return clone
 }
+
+function isNullish(x: unknown): x is null | undefined {
+  return x === null || x === undefined
+}
+
+export function min<T>(list: Array<T>, accessor?: (item: T) => number): number {
+  const values = list
+    .map(accessor ?? Number)
+    .filter(n => !(isNullish(n) || isNaN(n)))
+
+  return Math.min(...values)
+}
+
+export function max<T>(list: Array<T>, accessor?: (item: T) => number): number {
+  const values = list
+    .map(accessor ?? Number)
+    .filter(n => !(isNullish(n) || isNaN(n)))
+
+  return Math.max(...values)
+}
+
+export function sum<T>(list: Array<T>, accessor?: (item: T) => number): number {
+  return list.map(accessor ?? Number).reduce((sum, curr) => {
+    if (isNullish(curr) || isNaN(curr)) {
+      return sum
+    }
+
+    return sum + curr
+  }, 0)
+}
+
+export function groupBy<T>(
+  list: Array<T>,
+  group: (item: T) => number | string
+): Record<string, Array<T>> {
+  return list.reduce((groups, item) => {
+    const key = group(item)
+
+    if (groups[key]) {
+      groups[key].push(item)
+    } else {
+      groups[key] = [item]
+    }
+
+    return groups
+  }, {} as Record<string, Array<T>>)
+}
