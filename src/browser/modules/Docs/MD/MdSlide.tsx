0310323f99e91026e386720634bcdacf6f5b9017
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2022-03-02 17:21:32 +0100

    Fix tests and add clickable code

--- src/browser/modules/Docs/MD/MdSlide.tsx
+++ src/browser/modules/Docs/MD/MdSlide.tsx
@@ -30,7 +30,34 @@ const MdRow = ({ row = '' }: RowProps) => (
   <StyledRow>
     {splitMdColumns(row).map((column, index) => (
       <StyledColumn key={index}>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            pre({
+              className,
+              children,
+              ...props
+            }: React.DetailedHTMLProps<
+              React.HTMLAttributes<HTMLPreElement>,
+              HTMLPreElement
+            >) {
+              const isCypherCodeBlock =
+                Array.isArray(children) &&
+                children[0]?.props?.className?.includes('cypher')
+              return (
+                <pre
+                  className={
+                    (className ?? '') +
+                    (isCypherCodeBlock ? ' code runnable' : '')
+                  }
+                  {...props}
+                >
+                  {children}
+                </pre>
+              )
+            }
+          }}
+        >
           {column ?? ''}
         </ReactMarkdown>
       </StyledColumn>
