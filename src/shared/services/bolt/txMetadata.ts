876fe2ac239389650fca76b027d9e60849b2bbee
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2022-08-03 12:06:23 +0200

    Split version and name in metadata format

--- src/shared/services/bolt/txMetadata.ts
+++ src/shared/services/bolt/txMetadata.ts
@@ -24,25 +24,28 @@ export const NEO4J_BROWSER_BACKGROUND_QUERY = 'system'
 export const NEO4J_BROWSER_USER_QUERY = 'user-direct'
 export const NEO4J_BROWSER_USER_ACTION_QUERY = 'user-action'
 export const DEFAULT_QUERY_METADATA_TYPE = NEO4J_BROWSER_USER_ACTION_QUERY
-export const NEO4J_BROWSER_APP_ID = `neo4j-browser_v${version}`
+const NEO4J_BROWSER_APP_ID = 'neo4j-browser'
 
 export const backgroundTxMetadata = {
   txMetadata: {
     type: NEO4J_BROWSER_BACKGROUND_QUERY,
-    app: NEO4J_BROWSER_APP_ID
+    app: NEO4J_BROWSER_APP_ID,
+    version
   }
 }
 
 export const userDirectTxMetadata = {
   txMetadata: {
     type: NEO4J_BROWSER_USER_QUERY,
-    app: NEO4J_BROWSER_APP_ID
+    app: NEO4J_BROWSER_APP_ID,
+    version
   }
 }
 export const userActionTxMetadata = {
   txMetadata: {
     type: NEO4J_BROWSER_USER_QUERY,
-    app: NEO4J_BROWSER_APP_ID
+    app: NEO4J_BROWSER_APP_ID,
+    version
   }
 }
 export const defaultTxMetadata = userActionTxMetadata
@@ -52,6 +55,7 @@ export const getUserTxMetadata = (
 ) => ({
   txMetadata: {
     type,
-    app: NEO4J_BROWSER_APP_ID
+    app: NEO4J_BROWSER_APP_ID,
+    version
   }
 })
