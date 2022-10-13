3479f3d15bc021e28e1364cac8447fed2747ceda
Eija Werner <11065688+eijawerner@users.noreply.github.com>
2022-10-13 09:09:10 +0200

    Fixed broken auto scroll of properties pane (#1836)
    Also made sure that if there are many labels on one node, that it is scrollable instead of just rendered out of sight.

--- src/neo4j-arc/graph-visualization/GraphVisualizer/DefaultPanelContent/styled.tsx
+++ src/neo4j-arc/graph-visualization/GraphVisualizer/DefaultPanelContent/styled.tsx
@@ -46,15 +46,21 @@ export const NonClickableRelTypeChip = styled(StyledRelationshipChip)`
 
 export const PaneWrapper = styled.div`
   padding: 0 14px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
 export const PaneHeader = styled.div`
   font-size: 16px;
   margin-top: 10px;
   flex: 0 0 auto;
+  overflow: auto;
+  max-height: 50%;
 `
 
 export const PaneBody = styled.div`
+  height: 100%;
   overflow: auto;
   margin: 14px 0;
   flex: 0 1 auto;
