ed45d28921d2801dc590a0bfa21538dfbc733280
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-02-13 16:06:51 +0100

    make guides e2e test not  relay on remote

--- e2e_tests/integration/guide-command.spec.ts
+++ e2e_tests/integration/guide-command.spec.ts
@@ -87,8 +87,12 @@ describe('Guide command', () => {
     cy.get('[data-testid=navigationGuides]').click()
   })
 
-  it('can load and persist a remote guide and can be deleted permanantly', () => {
+  it.only('can load and persist a remote guide and can be deleted permanantly', () => {
     const guideUrl = 'https://guides.neo4j.com/sandbox/movies/index.html'
+    cy.intercept(guideUrl, {
+      fixture: 'getGuide.html'
+    }).as('getGuide')
+
     cy.executeCommand(':clear')
     cy.executeCommand(`:guide ${guideUrl}`, { timeout: 50000 })
     cy.get('[data-testid="guidesDrawer"]').should('contain', 'Movies Guide')
