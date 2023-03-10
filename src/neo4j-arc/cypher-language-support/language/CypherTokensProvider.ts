b9e3ebda0fdf16d9b532242c42b67878131b3e4b
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-03-10 10:51:51 +0100

    works but bad

--- src/neo4j-arc/cypher-language-support/language/CypherTokensProvider.ts
+++ src/neo4j-arc/cypher-language-support/language/CypherTokensProvider.ts
@@ -17,7 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { createCypherLexer } from '@neo4j-cypher/editor-support'
+import { CypherEditorSupport } from '@neo4j-cypher/editor-support'
+// @ts-ignore
+import { CypherLexer } from '@neo4j-cypher/antlr4'
 import { languages } from 'monaco-editor/esm/vs/editor/editor.api'
 
 class CypherState implements languages.IState {
@@ -30,31 +32,104 @@ class CypherState implements languages.IState {
   }
 }
 
+// TODO type export was from wrong folder
+export type CypherPart =
+  | 'label'
+  | 'variable'
+  | 'relationshipType'
+  | 'property'
+  | 'procedure'
+  | 'procedureOutput'
+  | 'function'
+  | 'function'
+  | 'parameter'
+  | 'consoleCommand'
+  | 'property'
+
+const lexer = new CypherLexer()
 export class CypherTokensProvider implements languages.TokensProvider {
   getInitialState(): CypherState {
     return new CypherState()
   }
+  v = 0
+  c = new CypherEditorSupport('')
 
-  tokenize(/* line: string */): languages.ILineTokens {
-    // TODO denna är sönder :( måste fixas innan vi kan använda den igen :( :( :(
-    // const lexer = createCypherLexer(line)
+  tokenize(line: string): languages.ILineTokens {
+    // @ts-ignore
+    this.c.update(line, this.v++)
+
+    const items: {
+      element: {
+        start: { start: number; stop: number; line: number; column: number }
+        stop: { start: number; stop: number; line: number; column: number }
+      }
+      type: CypherPart
+    }[] = []
+
+    // @ts-ignore
+    this.c.applyHighlighthing((element, type: CypherPart) => {
+      items.push({ element, type })
+    })
+    const all: any[] = []
+
+    function traverse(element: any) {
+      const c = element.getChildCount()
+      if (c === 0) {
+        all.push(element)
+
+        return
+      }
+      for (let i = 0; i < c; i += 1) {
+        traverse(element.getChild(i))
+      }
+    }
+
+    // @ts-ignore
+    traverse(this.c.parseTree)
+
+    // @ts-ignore
+    window.c = this.c
+
+    console.log(
+      items.map(item => ({
+        scopes: item.type,
+        startIndex: item.element.start.start,
+        endIndex: item.element.stop.stop
+      }))
+    )
+    console.log(items)
+    const fancyTokens = items.map(item => ({
+      scopes: item.type,
+      startIndex: item.element.start.column,
+      endIndex: item.element.stop.stop
+    }))
+
+    const basicTokens = all
+      .filter(t => t.symbol !== undefined && t.symbol.type !== -1)
+      .map(token => ({
+        scopes: (
+          CypherLexer.symbolicNames[token.symbol.type] ??
+          CypherLexer.literalNames[token.symbol.type] ??
+          ''
+        ).toLowerCase(),
+        startIndex: token.symbol.column
+      }))
+
+    const combined = basicTokens.filter(
+      basic =>
+        !fancyTokens.find(
+          f =>
+            basic.startIndex >= f.startIndex && basic.startIndex <= f.endIndex
+        )
+    )
+    console.log(basicTokens, combined, fancyTokens)
+
     return {
       endState: new CypherState(),
-
-      tokens: [] /* lexer
-        .getAllTokens()
-        .filter(token => token !== null && token.type !== -1)
-        .map(token => ({
-          scopes:
-            (
-              lexer.symbolicNames[token.type] ??
-              lexer.literalNames[token.type] ??
-              ''
-            ).toLowerCase() + '.cypher',
-          startIndex: token.column
-        }))
+      // @ts-ignore
+      tokens: combined
+        .concat(fancyTokens)
         .sort((a, b) => (a.startIndex > b.startIndex ? 1 : -1))
-        */
     }
   }
 }
