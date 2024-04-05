53514c978b9f66acee163b693b69688509a23a3a
Oskar Damkjær <OskarDamkjaer@users.noreply.github.com>
2024-04-05 16:49:41 +0200

    fix old syntax in favorite (#1965)

--- src/shared/modules/favorites/staticScripts.ts
+++ src/shared/modules/favorites/staticScripts.ts
@@ -96,7 +96,24 @@ REQUIRE n.<propertyKey> IS UNIQUE`,
     folder: 'profile',
     content:
       '// What kind of nodes exist\n// Sample some nodes, reporting on property and relationship counts per node.\nMATCH (n) WHERE rand() <= 0.1\nRETURN\nDISTINCT labels(n),\ncount(*) AS SampleSize,\navg(size(keys(n))) as Avg_PropertyCount,\nmin(size(keys(n))) as Min_PropertyCount,\nmax(size(keys(n))) as Max_PropertyCount,\navg(size( (n)-[]-() ) ) as Avg_RelationshipCount,\nmin(size( (n)-[]-() ) ) as Min_RelationshipCount,\nmax(size( (n)-[]-() ) ) as Max_RelationshipCount',
-    versionRange: '>=3'
+    versionRange: '>=3 <5'
+  },
+  {
+    folder: 'profile',
+    content: `// What kind of nodes exist
+// Sample some nodes, reporting on property and relationship counts per node.
+MATCH (n) WHERE rand() <= 0.1
+RETURN
+DISTINCT labels(n),
+count(*) AS SampleSize,
+avg(size(keys(n))) as Avg_PropertyCount,
+min(size(keys(n))) as Min_PropertyCount,
+max(size(keys(n))) as Max_PropertyCount,
+avg(count{ (n)-[]-() } ) as Avg_RelationshipCount,
+min(count{ (n)-[]-() } ) as Min_RelationshipCount,
+max(count{ (n)-[]-() } ) as Max_RelationshipCount
+`,
+    versionRange: '>=5'
   },
   {
     folder: 'profile',
