924814e8c33dab125913862898d683ede022c348
David Russell <david.russell@neo4j.com>
2024-10-30 17:41:17 +0000

    add pageload effecthook to app

--- src/browser/modules/App/App.tsx
+++ src/browser/modules/App/App.tsx
@@ -96,6 +96,7 @@ import {
   updateUdcData
 } from 'shared/modules/udc/udcDuck'
 import { getTelemetrySettings } from 'shared/utils/selectors'
+import { trackPageLoad } from 'shared/modules/preview/previewDuck'
 
 export const MAIN_WRAPPER_DOM_ID = 'MAIN_WRAPPER_DOM_ID'
 
@@ -148,6 +149,11 @@ export function App(props: any) {
     const initAction = udcInit()
     props.bus && props.bus.send(initAction.type, initAction)
   }, [props.bus])
+
+  useEffect(() => {
+    const pageLoadAction = trackPageLoad()
+    props.bus && props.bus.send(pageLoadAction.type, pageLoadAction)
+  }, [props.bus, props.telemetrySettings.allowUserStats])
 
   const {
     browserSyncAuthStatus,
