db9e63e532c96888134117c37f1388b409778854
michael-schienbein-fhr <m@schienbein.dev>
2024-12-03 03:30:14 -0600

    zZZZZZzzzz

--- src/browser/styles/themes.ts
+++ src/browser/styles/themes.ts
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { tokens } from '@neo4j-ndl/base/lib/tokens'
+import { tokens } from '@neo4j-ndl/base';
 import { baseArcTheme } from 'neo4j-arc/common'
 
 import {
