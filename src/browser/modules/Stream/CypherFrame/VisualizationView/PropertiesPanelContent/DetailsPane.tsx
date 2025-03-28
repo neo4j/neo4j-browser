3ead9615ebbfb3029e979a79b9b7692df7f54def
David Russell <david.russell@neo4j.com>
2025-03-28 13:47:23 +0000

    removes arc build/release configs and pipeline

--- src/browser/modules/Stream/CypherFrame/VisualizationView/PropertiesPanelContent/DetailsPane.tsx
+++ src/browser/modules/Stream/CypherFrame/VisualizationView/PropertiesPanelContent/DetailsPane.tsx
@@ -24,7 +24,7 @@ import { ClipboardCopier, PropertiesTable, upperFirst } from 'neo4j-arc/common'
 import { StyleableNodeLabel } from './StyleableNodeLabel'
 import { StyleableRelType } from './StyleableRelType'
 import { PaneBody, PaneHeader, PaneTitle, PaneWrapper } from './styled'
-import { DetailsPaneProps } from 'neo4j-arc'
+import { DetailsPaneProps } from 'neo4j-arc/graph-visualization'
 
 export const DETAILS_PANE_STEP_SIZE = 1000
 export function DetailsPane({
--- src/neo4j-arc/README.md
+++
@@ -1,7 +0,0 @@
-# Neo4j Arc - Browser Component Library
-
-This project currently contains all the components in common between Neo4j Browser and NX. Arc is a recursive backronym for Arc React Components.
-
-## Development
-
-Currently the only supported action for Arc is building it, a dev env & tests to come. To build arc, simply run `yarn build` in this directory, no other steps needed.
