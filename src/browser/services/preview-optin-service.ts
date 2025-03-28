b09d2070954dcd78e4613d430e0f2f510a5bb109
David Russell <david.russell@neo4j.com>
2025-03-28 11:56:33 +0000

    simplifies eu locale list

--- src/browser/services/preview-optin-service.ts
+++ src/browser/services/preview-optin-service.ts
@@ -1,113 +1,45 @@
-const EMEA_LOCALES = [
-  'bg-bg',
-  'cs-cz',
-  'cy-gb',
-  'da-dk',
-  'de-de',
-  'el-gr',
-  'en-gb',
-  'et-ee',
-  'fi-fi',
-  'fr-fr',
-  'ga-ie',
-  'hr-hr',
-  'hu-hu',
-  'is-is',
-  'it-it',
-  'lb-lu',
-  'lt-lt',
-  'lv-lv',
-  'mk-mk',
-  'mt-mt',
-  'nl-nl',
-  'no-no',
-  'pl-pl',
-  'pt-pt',
-  'ro-ro',
-  'ru-ru',
-  'sk-sk',
-  'sl-si',
-  'sr-rs',
-  'sv-se',
-  'uk-ua'
+const EU_LOCALES = [
+  'bg-bg', // Bulgarian (Bulgaria)
+  'cs-cz', // Czech (Czech Republic)
+  'cy-gb', // Welsh (United Kingdom)
+  'da-dk', // Danish (Denmark)
+  'de-at', // German (Austria)
+  'de-ch', // German (Switzerland)
+  'de-de', // German (Germany)
+  'el-gr', // Greek (Greece)
+  'en-gb', // English (United Kingdom)
+  'en-ie', // English (Ireland)
+  'et-ee', // Estonian (Estonia)
+  'fi-fi', // Finnish (Finland)
+  'fr-be', // French (Belgium)
+  'fr-ch', // French (Switzerland)
+  'fr-fr', // French (France)
+  'ga-ie', // Irish (Ireland)
+  'hr-hr', // Croatian (Croatia)
+  'hu-hu', // Hungarian (Hungary)
+  'is-is', // Icelandic (Iceland)
+  'it-ch', // Italian (Switzerland)
+  'it-it', // Italian (Italy)
+  'lb-lu', // Luxembourgish (Luxembourg)
+  'lt-lt', // Lithuanian (Lithuania)
+  'lv-lv', // Latvian (Latvia)
+  'mk-mk', // Macedonian (North Macedonia)
+  'mt-mt', // Maltese (Malta)
+  'nl-be', // Dutch (Belgium)
+  'nl-nl', // Dutch (Netherlands)
+  'no-no', // Norwegian (Norway)
+  'pl-pl', // Polish (Poland)
+  'pt-pt', // Portuguese (Portugal)
+  'ro-ro', // Romanian (Romania)
+  'ru-ru', // Russian (Russia)
+  'sk-sk', // Slovak (Slovakia)
+  'sl-si', // Slovenian (Slovenia)
+  'sr-rs', // Serbian (Serbia)
+  'sv-se', // Swedish (Sweden)
+  'uk-ua' // Ukrainian (Ukraine)
 ]
 
-const APAC_LOCALES = [
-  'as-in',
-  'az-az',
-  'bn-bd',
-  'bn-in',
-  'bo-cn',
-  'fil-ph',
-  'gu-in',
-  'hi-in',
-  'hy-am',
-  'id-id',
-  'ja-jp',
-  'ka-ge',
-  'km-kh',
-  'kn-in',
-  'ko-kr',
-  'lo-la',
-  'ml-in',
-  'mn-mn',
-  'mr-in',
-  'ms-my',
-  'my-mm',
-  'ne-np',
-  'or-in',
-  'pa-in',
-  'ps-af',
-  'sa-in',
-  'si-lk',
-  'ta-in',
-  'te-in',
-  'th-th',
-  'tr-tr',
-  'ug-cn',
-  'ur-in',
-  'ur-pk',
-  'vi-vn',
-  'zh-cn',
-  'zh-hk',
-  'zh-tw'
-]
-
-const AMERICAS_LOCALES = [
-  'ay-bo',
-  'en-029',
-  'en-ca',
-  'en-us',
-  'es-419',
-  'es-ar',
-  'es-bo',
-  'es-cl',
-  'es-co',
-  'es-cr',
-  'es-cu',
-  'es-do',
-  'es-ec',
-  'es-gq',
-  'es-gt',
-  'es-hn',
-  'es-mx',
-  'es-ni',
-  'es-pa',
-  'es-pe',
-  'es-pr',
-  'es-py',
-  'es-sv',
-  'es-uy',
-  'es-ve',
-  'fr-029',
-  'fr-ca',
-  'gn-py',
-  'pt-419',
-  'pt-br',
-  'qu-pe'
-]
-
-const OPTED_IN_LOCALES = [...EMEA_LOCALES]
+const OPTED_IN_LOCALES = [...EU_LOCALES]
 
 const userPrefersQuery = (): boolean => {
   const prefersOldBrowser = localStorage.getItem('prefersOldBrowser')
