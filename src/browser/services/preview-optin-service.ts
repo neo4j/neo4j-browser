997a3e8b06385033a47527f1c2f002c18b919372
David Russell <david.russell@neo4j.com>
2025-03-31 08:59:42 +0100

    opt in users by utc offset

--- src/browser/services/preview-optin-service.ts
+++ src/browser/services/preview-optin-service.ts
@@ -1,55 +1,12 @@
-const EU_LOCALES = [
-  'bg-bg', // Bulgarian (Bulgaria)
-  'cs-cz', // Czech (Czech Republic)
-  'cy-gb', // Welsh (United Kingdom)
-  'da-dk', // Danish (Denmark)
-  'de-at', // German (Austria)
-  'de-ch', // German (Switzerland)
-  'de-de', // German (Germany)
-  'el-gr', // Greek (Greece)
-  'en-gb', // English (United Kingdom)
-  'en-ie', // English (Ireland)
-  'et-ee', // Estonian (Estonia)
-  'fi-fi', // Finnish (Finland)
-  'fr-be', // French (Belgium)
-  'fr-ch', // French (Switzerland)
-  'fr-fr', // French (France)
-  'ga-ie', // Irish (Ireland)
-  'hr-hr', // Croatian (Croatia)
-  'hu-hu', // Hungarian (Hungary)
-  'is-is', // Icelandic (Iceland)
-  'it-ch', // Italian (Switzerland)
-  'it-it', // Italian (Italy)
-  'lb-lu', // Luxembourgish (Luxembourg)
-  'lt-lt', // Lithuanian (Lithuania)
-  'lv-lv', // Latvian (Latvia)
-  'mk-mk', // Macedonian (North Macedonia)
-  'mt-mt', // Maltese (Malta)
-  'nl-be', // Dutch (Belgium)
-  'nl-nl', // Dutch (Netherlands)
-  'no-no', // Norwegian (Norway)
-  'pl-pl', // Polish (Poland)
-  'pt-pt', // Portuguese (Portugal)
-  'ro-ro', // Romanian (Romania)
-  'ru-ru', // Russian (Russia)
-  'sk-sk', // Slovak (Slovakia)
-  'sl-si', // Slovenian (Slovenia)
-  'sr-rs', // Serbian (Serbia)
-  'sv-se', // Swedish (Sweden)
-  'uk-ua' // Ukrainian (Ukraine)
-]
-
-const OPTED_IN_LOCALES = [...EU_LOCALES]
-
 const userHasNotOptedOutOfPreview = (): boolean => {
   const prefersOldBrowser = localStorage.getItem('prefersOldBrowser')
   const doesPreferQuery = prefersOldBrowser === 'false'
   return doesPreferQuery || prefersOldBrowser === null
 }
 
-export const optedInByRegion = (): boolean => {
-  return (
-    OPTED_IN_LOCALES.includes(navigator.language.toLowerCase()) &&
-    userHasNotOptedOutOfPreview()
-  )
+export const optedInByUtcOffset = (): boolean => {
+  const utcOffset = new Date().getTimezoneOffset()
+  // UTC offset of -120 minutes (2 hours) e.g. CEST to 0 minutes (0 hours)
+  const utcOffsetIsOptedIn = utcOffset >= -120 && utcOffset <= 0
+  return utcOffsetIsOptedIn && userHasNotOptedOutOfPreview()
 }
